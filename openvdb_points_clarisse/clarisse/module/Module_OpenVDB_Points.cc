f28f47a7a44326eb602cf53dd3f9f30e8bb37f35
Dan Bailey <dan@dneg.com>
2015-10-14 18:53:31 -0700

    New Clarisse ray-tracing module introduced - openvdb_points_clarisse

---
+++ openvdb_points_clarisse/clarisse/module/Module_OpenVDB_Points.cc
@@ -0,0 +1,381 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015 Double Negative Visual Effects
+//
+// All rights reserved. This software is distributed under the
+// Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
+//
+// Redistributions of source code must retain the above copyright
+// and license notice and the following restrictions and disclaimer.
+//
+// *     Neither the name of Double Negative Visual Effects nor the names
+// of its contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDERS' AND CONTRIBUTORS' AGGREGATE
+// LIABILITY FOR ALL CLAIMS REGARDLESS OF THEIR BASIS EXCEED US$250.00.
+//
+///////////////////////////////////////////////////////////////////////////
+//
+/// @file Module_OpenVDB_Points.cc
+///
+/// @author Dan Bailey
+///
+/// @brief OpenVDB Points Module for Clarisse
+///
+
+#include <openvdb/openvdb.h>
+#include <openvdb/Types.h>
+#include <openvdb_points/openvdb.h>
+#include <openvdb_points/tools/PointDataGrid.h>
+
+#include <iostream>
+
+#include <boost/algorithm/string/predicate.hpp>
+
+#include <dso_export.h>
+#include <module_geometry.h>
+#include <module_object.h>
+#include <app_object.h>
+#include <of_app.h>
+#include <of_class.h>
+#include <sys_filesystem.h>
+#include <app_progress_bar.h>
+
+#include "Geometry_OpenVDBPoints.cma"
+
+#include "ResourceData_OpenVDBPoints.h"
+#include "Geometry_OpenVDBPoints.h"
+
+
+////////////////////////////////////////
+
+
+IX_BEGIN_DECLARE_MODULE_CALLBACKS(OpenVDBPoints, ModuleGeometryCallbacks)
+    static void init_class(OfClass& cls);
+    static ResourceData* create_resource(OfObject&, const int&, void*);
+    static void* create_thread_data(const OfObject& object, const CtxEval& eval_ctx);
+    static void destroy_thread_data(const OfObject& object, const CtxEval& eval_ctx, void *thread_data);
+    static void on_attribute_change(OfObject&, const OfAttr&, int&, const int&);
+    static bool on_time_change(OfObject&, const double&);
+    static void get_grid_tag_candidates(const OfObject&, const OfAttr&, CoreVector<CoreString>&, CoreArray<bool>&);
+IX_END_DECLARE_MODULE_CALLBACKS(OpenVDBPoints)
+
+
+////////////////////////////////////////
+
+
+void registerVDBPoints(OfApp& app, CoreVector<OfClass *>& new_classes)
+{
+    // Geometry VDB Points
+
+    OfClass* new_class = IX_DECLARE_MODULE_CLASS(OpenVDBPoints);
+    new_classes.add(new_class);
+
+    IX_MODULE_CLBK* module_callbacks;
+    IX_CREATE_MODULE_CLBK(new_class, module_callbacks)
+    IX_MODULE_CLBK::init_class(*new_class);
+
+    IX_INIT_CLASS_TAG_CALLBACK(new_class, grid)
+
+    module_callbacks->cb_on_attribute_change = IX_MODULE_CLBK::on_attribute_change;
+    module_callbacks->cb_create_resource = IX_MODULE_CLBK::create_resource;
+    module_callbacks->cb_create_thread_data = IX_MODULE_CLBK::create_thread_data;
+    module_callbacks->cb_destroy_thread_data = IX_MODULE_CLBK::destroy_thread_data;
+    module_callbacks->cb_on_new_time = IX_MODULE_CLBK::on_time_change;
+}
+
+
+////////////////////////////////////////
+
+
+namespace resource
+{
+    enum {
+        RESOURCE_ID_VDB_GRID = ModuleGeometry::RESOURCE_ID_COUNT,
+    };
+} // namespace resource
+
+
+////////////////////////////////////////
+
+
+// Callback for initialising the resources and their dependencies
+
+void
+IX_MODULE_CLBK::init_class(OfClass& cls)
+{
+    openvdb::initialize();
+    openvdb::points::initialize();
+
+    CoreVector<CoreString> data_attrs;
+    CoreVector<int> deps;
+
+    data_attrs.add("filename");
+    data_attrs.add("grid");
+    cls.add_resource(resource::RESOURCE_ID_VDB_GRID, data_attrs, deps, "vdb_points_data");
+
+    CoreVector<CoreString> geo_attrs;
+    geo_attrs.add("explicit_radius");
+    geo_attrs.add("override_radius");
+    geo_attrs.add("radius_scale");
+
+    deps.add(resource::RESOURCE_ID_VDB_GRID);
+    cls.set_resource_attrs(ModuleGeometry::RESOURCE_ID_GEOMETRY, geo_attrs);
+    cls.set_resource_deps(ModuleGeometry::RESOURCE_ID_GEOMETRY, deps);
+
+    deps.add(ModuleGeometry::RESOURCE_ID_GEOMETRY);
+    cls.set_resource_attrs(ModuleGeometry::RESOURCE_ID_GEOMETRY_PROPERTIES, geo_attrs);
+    cls.set_resource_deps(ModuleGeometry::RESOURCE_ID_GEOMETRY_PROPERTIES, deps);
+}
+
+
+// Callback for populating the grid names attribute
+
+void
+IX_MODULE_CLBK::get_grid_tag_candidates(const OfObject& object,
+                                        const OfAttr& attr,
+                                        CoreVector<CoreString>& candidates,
+                                        CoreArray<bool>& preset_hints)
+{
+    CoreString filename = object.get_attribute("output_filename")->get_string();
+
+    // early exit if filename isn't set or does not exist on disk
+
+    if (filename.is_empty())                    return;
+    if (!SysFilesystem::file_exists(filename))  return;
+
+    // load the VDB metadata and update the candidate array with grid names
+
+    openvdb::io::File file(filename.get_data());
+    file.open();
+
+    openvdb::GridPtrVecPtr grids = file.readAllGridMetadata();
+    for (openvdb::GridPtrVecCIter iter = grids->begin(); iter != grids->end(); ++iter)
+    {
+        openvdb::tools::PointDataGrid::ConstPtr grid = openvdb::gridPtrCast<openvdb::tools::PointDataGrid>(*iter);
+
+        if (grid)   candidates.add(grid->getName().c_str());
+    }
+
+    file.close();
+}
+
+
+// Callback to update filename and radius attributes
+
+void
+IX_MODULE_CLBK::on_attribute_change(OfObject& object,
+                                    const OfAttr& attr,
+                                    int& dirtiness,
+                                    const int& dirtiness_flags)
+{
+    CoreString attr_name = attr.get_name();
+    if (attr_name == "filename")
+    {
+        const std::string filename = object.get_attribute("filename")->get_string().get_data();
+        object.get_attribute("output_filename")->set_string(filename.c_str());
+    }
+    else if (attr_name == "override_radius")
+    {
+        if (attr.get_bool())
+        {
+            object.get_attribute("explicit_radius")->set_read_only(false);
+            object.get_attribute("radius_scale")->set_read_only(true);
+        }
+        else
+        {
+            object.get_attribute("explicit_radius")->set_read_only(true);
+            object.get_attribute("radius_scale")->set_read_only(false);
+        }
+    }
+}
+
+
+// Callback to refresh the (hidden) filename when the frame changes
+
+bool
+IX_MODULE_CLBK::on_time_change(OfObject& object, const double& time)
+{
+    const std::string filename = object.get_attribute("filename")->get_string().get_data();
+    object.get_attribute("output_filename")->set_string(filename.c_str());
+    return true;
+}
+
+
+// Callback to create a PointDataAccessor per thread for efficiency
+
+void*
+IX_MODULE_CLBK::create_thread_data(const OfObject& object, const CtxEval& eval_ctx)
+{
+    ModuleGeometry* module = (ModuleGeometry*) object.get_module();
+    ResourceData_OpenVDBPoints* data = (ResourceData_OpenVDBPoints*) module->get_resource(resource::RESOURCE_ID_VDB_GRID);
+    return data ? data->create_thread_data() : 0;
+}
+
+
+// Callback to destroy a PointDataAccessor per thread
+
+void
+IX_MODULE_CLBK::destroy_thread_data(const OfObject& object, const CtxEval& eval_ctx, void *thread_data)
+{
+    ModuleGeometry *module = (ModuleGeometry *)object.get_module();
+    ResourceData_OpenVDBPoints *data = (ResourceData_OpenVDBPoints *)module->get_resource(resource::RESOURCE_ID_VDB_GRID);
+    if (data && thread_data) {
+        data->destroy_thread_data(thread_data);
+    }
+}
+
+
+// Create the resources
+
+ResourceData*
+IX_MODULE_CLBK::create_resource(OfObject& object,
+                                const int& resource_id,
+                                void* data)
+{
+    if (resource_id == resource::RESOURCE_ID_VDB_GRID)
+    {
+        const CoreString filename = object.get_attribute("output_filename")->get_string();
+        const CoreString gridname = object.get_attribute("grid")->get_string();
+
+        // early exit if file does not exist on disk
+
+        if (!SysFilesystem::file_exists(filename))      return 0;
+
+        AppProgressBar* read_progress_bar = object.get_application().create_progress_bar(CoreString("Loading VDB Points Data: ") + filename + " " + gridname);
+
+        ResourceData_OpenVDBPoints* resourceData;
+
+        // attempt to open and load the VDB grid from the file
+
+        openvdb::tools::PointDataGrid::Ptr grid;
+
+        int error = 0;
+
+        try {
+            grid = openvdb_points::load(filename.get_data(), gridname.get_data());
+        }
+        catch (const openvdb::IoError& e) {
+            std::cerr << "ERROR: Unable to open VDB file (" << filename.get_data() << "): " << e.what() << "\n";
+            error = 1;
+        }
+        catch (const openvdb::KeyError& e) {
+            std::cerr << "ERROR: Unable to retrieve grid (" << gridname.get_data() << ") from VDB file: " << e.what() << "\n";
+            error = 1;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "ERROR: Unknown error accessing (" << gridname.get_data() << "): " << e.what() << "\n";
+            error = 1;
+        }
+
+        if (!grid)  error = 1;
+
+        read_progress_bar->destroy();
+
+        if (error)  return 0;
+
+        // localising velocity and radius
+
+        AppProgressBar* localise_progress_bar = object.get_application().create_progress_bar(CoreString("Localising Velocity and Radius for ") + gridname);
+
+        openvdb_points::localise(grid);
+
+        localise_progress_bar->destroy();
+
+        // create the resource
+
+        resourceData = ResourceData_OpenVDBPoints::create(grid);
+
+        // if radius is available on the grid, enable the option to override it
+
+        if (resourceData->attribute_type("pscale") == "") {
+            object.get_attribute("override_radius")->set_bool(true);
+            object.get_attribute("override_radius")->set_read_only(true);
+        }
+        else {
+            object.get_attribute("override_radius")->set_read_only(false);
+        }
+
+        return resourceData;
+    }
+    else if (resource_id == ModuleGeometry::RESOURCE_ID_GEOMETRY)
+    {
+        ModuleGeometry* module = (ModuleGeometry*) object.get_module();
+        ResourceData_OpenVDBPoints* data = (ResourceData_OpenVDBPoints*) module->get_resource(resource::RESOURCE_ID_VDB_GRID);
+
+        if (!data)  return 0;
+
+        const bool override_radius = object.get_attribute("override_radius")->get_bool();
+        const double radius = object.get_attribute("explicit_radius")->get_double();
+        const double radius_scale = object.get_attribute("radius_scale")->get_double();
+
+        openvdb::tools::PointDataGrid::Ptr grid = data->grid();
+
+        if (!grid)  return 0;
+        if (!grid->tree().cbeginLeaf())     return 0;
+
+        openvdb::tools::PointDataTree& tree = grid->tree();
+
+        // check descriptor for velocity and radius (pscale)
+
+        openvdb::tools::PointDataTree::LeafCIter iter = grid->tree().cbeginLeaf();
+
+        assert(iter);
+
+        const openvdb::tools::AttributeSet::Descriptor& descriptor = iter->attributeSet().descriptor();
+
+        // retrieve fps, motion blur length and direction
+
+        const OfApp& application = object.get_application();
+
+        const double fps = object.get_factory().get_time().get_fps();
+        const double motionBlurLength = application.get_motion_blur_length();
+        const int motionBlurDirection = application.get_motion_blur_direction();
+        const AppBase::MotionBlurDirectionMode motionBlurMode =
+                            application.get_motion_blur_direction_mode_from_value(motionBlurDirection);
+
+        const double radiusArg = override_radius ? radius : radius_scale;
+
+        Geometry_OpenVDBPoints* geometry = Geometry_OpenVDBPoints::create(  grid, override_radius, radiusArg,
+                                                                            fps, motionBlurLength, motionBlurMode);
+
+        if (geometry)
+        {
+            AppProgressBar* acc_progress_bar = object.get_application().create_progress_bar(CoreString("Computing VDB Point Acceleration Structures"));
+
+            geometry->computeAccelerationStructures();
+
+            acc_progress_bar->destroy();
+        }
+
+        return geometry;
+    }
+
+    return 0;
+}
+
+
+////////////////////////////////////////
+
+
+// Register the plugin
+
+IX_BEGIN_EXTERN_C
+    DSO_EXPORT void
+    on_register_module(OfApp& app, CoreVector<OfClass *>& new_classes)
+    {
+        registerVDBPoints(app, new_classes);
+    }
+IX_END_EXTERN_C
---
+++ openvdb_points_clarisse/clarisse/module/ResourceData_OpenVDBPoints.cc
@@ -0,0 +1,323 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015 Double Negative Visual Effects
+//
+// All rights reserved. This software is distributed under the
+// Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
+//
+// Redistributions of source code must retain the above copyright
+// and license notice and the following restrictions and disclaimer.
+//
+// *     Neither the name of Double Negative Visual Effects nor the names
+// of its contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDERS' AND CONTRIBUTORS' AGGREGATE
+// LIABILITY FOR ALL CLAIMS REGARDLESS OF THEIR BASIS EXCEED US$250.00.
+//
+///////////////////////////////////////////////////////////////////////////
+//
+/// @file ResourceData_OpenVDBPoints.cc
+///
+/// @author Dan Bailey
+
+
+#include <app_base.h>
+#include <app_progress_bar.h>
+
+#include <openvdb/tools/Prune.h>
+#include <openvdb_points/tools/AttributeArray.h>
+
+#include "ResourceData_OpenVDBPoints.h"
+
+using namespace openvdb;
+using namespace openvdb::tools;
+
+
+////////////////////////////////////////
+
+
+namespace openvdb_points
+{
+
+PointDataGrid::Ptr
+load(   const std::string& filename,
+        const std::string& gridname,
+        const bool doPrune)
+{
+    // early exit if filename or gridname are blank
+
+    if (filename == "" || gridname == "")   return PointDataGrid::Ptr();
+
+    GridBase::Ptr gridBase;
+    io::File file(filename);
+    file.open();
+
+    gridBase = file.readGrid(gridname);
+
+    file.close();
+
+    PointDataGrid::Ptr grid = gridPtrCast<PointDataGrid>(gridBase);
+
+    // @todo: re-introduce pruning
+    //if (doPrune)  prune(grid);
+
+    return grid;
+}
+
+
+////////////////////////////////////////
+
+
+namespace resource_data_internal
+{
+
+template <typename VelocityType>
+struct ConvertVelocityToIndexSpaceOp {
+
+    typedef tree::LeafManager<PointDataTree> LeafManager;
+    typedef PointDataTree::LeafNodeType PointDataLeafNode;
+
+    ConvertVelocityToIndexSpaceOp(  const PointDataTree& tree,
+                                    const size_t velocityIndex,
+                                    const math::Transform& transform)
+        : mAccessor(tree)
+        , mIndex(velocityIndex)
+        , mTransform(transform) { }
+
+    void operator()(const LeafManager::LeafRange& range) const {
+
+        for (LeafManager::LeafRange::Iterator leaf=range.begin(); leaf; ++leaf) {
+
+            typename AttributeWriteHandle<VelocityType>::Ptr velocityWriteHandle =
+                leaf->attributeWriteHandle<VelocityType>(mIndex);
+
+            // @todo: need to extend the AttributeWriteHandle API to support uniform values
+
+            // if (velocityWriteHandle.isUniform()) {
+            //     const VelocityType velocity = mTransform.worldToIndex(velocityWriteHandle.get(Index64(0)));
+            //     velocityWriteHandle.collapse(transformedVelocity);
+            // }
+            // else {
+                for (PointDataLeafNode::ValueOnCIter value = leaf->cbeginValueOn(); value; ++value)
+                {
+                    Coord ijk = value.getCoord();
+
+                    if(mAccessor.pointCount(ijk) == 0) continue;
+
+                    PointDataAccessor<PointDataTree>::PointDataIndex range = mAccessor.get(ijk);
+
+                    const unsigned start = range.first;
+                    const unsigned end = range.second;
+
+                    for (unsigned index = start; index < end; index++) {
+
+                        const VelocityType velocity = velocityWriteHandle->get(Index64(index));
+
+                        const VelocityType transformedVelocity = mTransform.worldToIndex(velocity);
+
+                        velocityWriteHandle->set(Index64(index), transformedVelocity);
+                    }
+                }
+            // }
+        }
+    }
+
+    //////////
+
+    PointDataAccessor<PointDataTree>    mAccessor;
+    const unsigned                      mIndex;
+    const math::Transform&              mTransform;
+}; // ConvertVelocityToIndexSpaceOp
+
+template <typename ScalarType>
+struct ConvertScalarToIndexSpaceOp {
+
+    typedef tree::LeafManager<PointDataTree> LeafManagerT;
+    typedef PointDataTree::LeafNodeType PointDataLeafNode;
+
+    ConvertScalarToIndexSpaceOp(const PointDataTree& tree,
+                                const size_t index,
+                                const math::Transform& transform)
+        : mAccessor(tree)
+        , mIndex(index)
+        , mTransform(transform) { }
+
+    void operator()(const LeafManagerT::LeafRange& range) const {
+
+        for (LeafManagerT::LeafRange::Iterator leaf=range.begin(); leaf; ++leaf) {
+
+            typename AttributeWriteHandle<ScalarType>::Ptr scalarWriteHandle =
+                leaf->attributeWriteHandle<ScalarType>(mIndex);
+
+            // @todo: need to extend the AttributeWriteHandle API to support uniform values
+
+            // if (scalarWriteHandle.isUniform()) {
+            //     const ScalarType transformedScalar = scalarWriteHandle.get(Index64(0)) / mTransform.voxelSize()[0];
+            //     scalarWriteHandle.collapse(transformedScalar);
+            // }
+            // else {
+                for (PointDataLeafNode::ValueOnCIter value = leaf->cbeginValueOn(); value; ++value)
+                {
+                    Coord ijk = value.getCoord();
+
+                    if(mAccessor.pointCount(ijk) == 0) continue;
+
+                    PointDataAccessor<PointDataTree>::PointDataIndex range = mAccessor.get(ijk);
+
+                    const unsigned start = range.first;
+                    const unsigned end = range.second;
+
+                    for (unsigned index = start; index < end; index++) {
+
+                        const ScalarType scalar = scalarWriteHandle->get(Index64(index));
+
+                        const ScalarType transformedScalar = scalar / mTransform.voxelSize()[0];
+
+                        scalarWriteHandle->set(Index64(index), transformedScalar);
+                    }
+                }
+            // }
+        }
+    }
+
+    //////////
+
+    PointDataAccessor<PointDataTree>    mAccessor;
+    const unsigned                      mIndex;
+    const math::Transform&              mTransform;
+}; // ConvertScalarToIndexSpaceOp
+
+} // resource_data_internal
+
+
+////////////////////////////////////////
+
+
+void localise(PointDataGrid::Ptr& grid)
+{
+    PointDataTree& tree = grid->tree();
+    PointDataTree::LeafCIter iter = tree.cbeginLeaf();
+    math::Transform& transform = grid->transform();
+
+    // early exit if no points
+
+    if (!iter)  return;
+
+    const AttributeSet::Descriptor& descriptor = iter->attributeSet().descriptor();
+
+    // obtain leaf range
+
+    typedef tree::LeafManager<PointDataTree> LeafManager;
+    typedef LeafManager::LeafRange LeafRange;
+
+    LeafManager manager(tree);
+    LeafRange range = manager.leafRange();
+
+    using resource_data_internal::ConvertVelocityToIndexSpaceOp;
+    using resource_data_internal::ConvertScalarToIndexSpaceOp;
+
+    // localise velocity
+
+    const size_t velocityIndex = descriptor.find("v");
+
+    if (velocityIndex != AttributeSet::INVALID_POS)
+    {
+        const NamePair& type = descriptor.type(velocityIndex);
+
+        if (type.first == "vec3s") {
+            tbb::parallel_for(range, ConvertVelocityToIndexSpaceOp<Vec3f>(tree, velocityIndex, transform));
+        }
+        else if (type.first == "vec3h") {
+            tbb::parallel_for(range, ConvertVelocityToIndexSpaceOp<math::Vec3<half> >(tree, velocityIndex, transform));
+        }
+        else {
+            std::cerr << "Unsupported type for velocity - " << type.first << std::endl;
+        }
+    }
+
+    // localise radius
+
+    const size_t pscaleIndex = descriptor.find("pscale");
+
+    if (pscaleIndex != AttributeSet::INVALID_POS)
+    {
+        const NamePair& type = descriptor.type(pscaleIndex);
+
+        if (type.first == "float") {
+            tbb::parallel_for(range, ConvertScalarToIndexSpaceOp<float>(tree, pscaleIndex, transform));
+        }
+        else if (type.first == "half") {
+            tbb::parallel_for(range, ConvertScalarToIndexSpaceOp<half>(tree, pscaleIndex, transform));
+        }
+        else {
+            std::cerr << "Unsupported type for pscale - " << type.first << std::endl;
+        }
+    }
+}
+
+} // namespace openvdb_points
+
+
+////////////////////////////////////////
+
+
+ResourceData_OpenVDBPoints*
+ResourceData_OpenVDBPoints::create(const PointDataGrid::Ptr& grid)
+{
+    return new ResourceData_OpenVDBPoints(grid);
+}
+
+
+ResourceData_OpenVDBPoints::ResourceData_OpenVDBPoints(const PointDataGrid::Ptr& grid)
+    : m_grid(grid)
+    , m_descriptor(grid->tree().cbeginLeaf()->attributeSet().descriptorPtr())
+{
+}
+
+
+void*
+ResourceData_OpenVDBPoints::create_thread_data() const
+{
+    PointDataAccessor<PointDataTree>* accessor = new PointDataAccessor<PointDataTree>(m_grid->tree());
+
+    return accessor;
+}
+
+
+void
+ResourceData_OpenVDBPoints::destroy_thread_data(void *data) const
+{
+    delete static_cast<PointDataAccessor<PointDataTree>*>(data);
+}
+
+
+size_t
+ResourceData_OpenVDBPoints::get_memory_size() const
+{
+    return sizeof(*this) + m_grid->memUsage();
+}
+
+
+std::string
+ResourceData_OpenVDBPoints::attribute_type(const std::string& name) const
+{
+    if (!m_descriptor)   return "";
+
+    const size_t pos = m_descriptor->find(name);
+
+    if (pos == AttributeSet::INVALID_POS)   return "";
+
+    return m_descriptor->type(pos).first;
+}
