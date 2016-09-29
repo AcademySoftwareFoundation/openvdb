a15884e066bb939477121b53db3950e929575b7d
plattfot <fredriks@d2.com>
2016-09-29 14:36:32 -0700

    Removed some commented out code

--- if_houdini_ge_15.mk
+++ if_houdini_ge_15.mk
@@ -4,7 +4,5 @@ ifeq ($(hou_ge_15),true)
   GCC_HOME:=/tools/package/gcc/$(GCC_VERSION)
   CXX:=$(GCC_HOME)/bin/g++
   CXXFLAGS += -std=c++11
-  #GCC_LIB_DIR := $(ICC_HOME)/compiler/lib/intel64
-  #let the makefile know we're using icc
   using_newer_gcc := yes
 endif
