cd3958e5431f96576f151ebb3e06ee5179d2176d
Dan Bailey <dan@dneg.com>
2016-11-14 14:45:49 -0800

    fix trailing space

--- openvdb_houdini/houdini/GEO_VDBTranslator.cc
+++ openvdb_houdini/houdini/GEO_VDBTranslator.cc
@@ -173,7 +173,7 @@ GEO_VDBTranslator::fileStat(const char *filename, GA_Stat &stat, uint level)
 	bbox.makeInvalid();
 
         // Loop over all grids in the file.
-        for (openvdb::io::File::NameIterator nameIter = file.beginName(); nameIter != file.endName(); ++nameIter) 
+        for (openvdb::io::File::NameIterator nameIter = file.beginName(); nameIter != file.endName(); ++nameIter)
 	{
             const std::string& gridName = nameIter.gridName();
 
