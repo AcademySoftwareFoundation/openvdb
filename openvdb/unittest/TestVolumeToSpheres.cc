750006a49284ba329d8bb3d934229eca94187dc4
Ken Museth <ken.museth@dreamworks.com>
2016-11-15 17:39:18 -0800

    Public release of v4.0.0

---
+++ openvdb/unittest/TestVolumeToSpheres.cc
@@ -0,0 +1,146 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2012-2016 DreamWorks Animation LLC
+//
+// All rights reserved. This software is distributed under the
+// Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
+//
+// Redistributions of source code must retain the above copyright
+// and license notice and the following restrictions and disclaimer.
+//
+// *     Neither the name of DreamWorks Animation nor the names of
+// its contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDERS' AND CONTRIBUTORS' AGGREGATE
+// LIABILITY FOR ALL CLAIMS REGARDLESS OF THEIR BASIS EXCEED US$250.00.
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <openvdb/openvdb.h> // for FloatGrid
+#include <openvdb/tools/LevelSetSphere.h>// for createLevelSetSphere
+#include <openvdb/tools/LevelSetUtil.h>// for sdfToFogVolume
+#include <openvdb/tools/VolumeToSpheres.h>// for fillWithSpheres
+#include <openvdb/Exceptions.h>
+
+#include <vector>
+
+class TestVolumeToSphere: public CppUnit::TestCase
+{
+public:
+    CPPUNIT_TEST_SUITE(TestVolumeToSphere);
+    CPPUNIT_TEST(testFromLevelSet);
+    CPPUNIT_TEST(testFromFog);
+    CPPUNIT_TEST_SUITE_END();
+
+    void testFromLevelSet();
+    void testFromFog();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestVolumeToSphere);
+
+
+////////////////////////////////////////
+
+
+void
+TestVolumeToSphere::testFromLevelSet()
+{
+    const float radius = 20.0f, voxelSize = 1.0f, halfWidth = 3.0f;
+    const openvdb::Vec3f center(15.0f, 13.0f, 16.0f);
+
+    openvdb::FloatGrid::ConstPtr grid =
+        openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(radius, center,
+                                                                 voxelSize, halfWidth);
+
+    std::vector<openvdb::Vec4s> spheres;
+    const int maxSphereCount = 100, instanceCount = 10000;
+    const bool overlapping = false;
+    const float minRadius = 5.0, maxRadius = std::numeric_limits<float>::max(), isovalue = 0.0;
+
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+
+    openvdb::tools::fillWithSpheres(*grid, spheres, maxSphereCount, overlapping,
+                                    minRadius, maxRadius, isovalue, instanceCount);
+
+    CPPUNIT_ASSERT_EQUAL(1, int(spheres.size()));
+
+    //for (size_t i=0; i< spheres.size(); ++i) {
+    //    std::cout << "\nSphere #" << i << ": " << spheres[i] << std::endl;
+    //}
+
+    const auto tolerance = 2.0 * voxelSize;
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[0], spheres[0][0], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[1], spheres[0][1], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[2], spheres[0][2], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(radius,    spheres[0][3], tolerance);
+
+    spheres.clear();
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+
+    // Now pick an iso-value that is outside of the narrow band (this used to crash!)
+    openvdb::tools::fillWithSpheres(*grid, spheres,maxSphereCount,
+                                    overlapping,minRadius, maxRadius, 1.5*halfWidth, instanceCount);
+
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+}
+
+void
+TestVolumeToSphere::testFromFog()
+{
+    const float radius = 20.0f, voxelSize = 1.0f, halfWidth = 3.0f;
+    const openvdb::Vec3f center(15.0f, 13.0f, 16.0f);
+
+    openvdb::FloatGrid::Ptr grid =
+        openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(radius, center,
+                                                                 voxelSize, halfWidth);
+    openvdb::tools::sdfToFogVolume(*grid);
+
+    std::vector<openvdb::Vec4s> spheres;
+    const int maxSphereCount = 100, instanceCount = 10000;
+    const bool overlapping = false;
+    const float minRadius = 5.0, maxRadius = std::numeric_limits<float>::max(), isovalue = 0.01f;
+
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+
+    openvdb::tools::fillWithSpheres(*grid, spheres, maxSphereCount, overlapping,
+                                    minRadius, maxRadius, isovalue, instanceCount);
+
+    //for (size_t i=0; i< spheres.size(); ++i) {
+    //    std::cout << "\nSphere #" << i << ": " << spheres[i] << std::endl;
+    //}
+
+    CPPUNIT_ASSERT_EQUAL(1, int(spheres.size()));
+
+    const auto tolerance = 2.0 * voxelSize;
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[0], spheres[0][0], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[1], spheres[0][1], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(center[2], spheres[0][2], tolerance);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(radius,    spheres[0][3], tolerance);
+
+    spheres.clear();
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+
+    // Now pick an iso-value that is outside of the narrow band (this used tocrash!)
+    openvdb::tools::fillWithSpheres(*grid, spheres,maxSphereCount,
+                                    overlapping,minRadius, maxRadius, 0.0f, instanceCount);
+
+    CPPUNIT_ASSERT_EQUAL(0, int(spheres.size()));
+}
+
+// Copyright (c) 2012-2016 DreamWorks Animation LLC
+// All rights reserved. This software is distributed under the
+// Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
