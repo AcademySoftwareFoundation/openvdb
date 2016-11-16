3e7c38b868ba5d61742e0f0ac65efa7365f0e9b2
Dan Bailey <dan@dneg.com>
2016-11-16 07:40:48 -0800

    Add points::initialize() to ensure VDB Point grids can be read by default

--- openvdb/openvdb.cc
+++ openvdb/openvdb.cc
@@ -30,6 +30,7 @@
 
 #include "openvdb.h"
 #include <openvdb/tools/PointIndexGrid.h>
+#include <openvdb/points/PointDataGrid.h>
 #include <tbb/mutex.h>
 #ifdef OPENVDB_USE_LOG4CPLUS
 #include <log4cplus/configurator.h>
@@ -115,6 +116,9 @@ initialize()
     Metadata::registerType(typeNameAsString<PointIndex32>(), Int32Metadata::createMetadata);
     Metadata::registerType(typeNameAsString<PointIndex64>(), Int64Metadata::createMetadata);
     tools::PointIndexGrid::registerGrid();
+
+    // Register types associated with point data grids.
+    openvdb::points::initialize();
 
 #ifdef OPENVDB_USE_BLOSC
     blosc_init();
