a3bb0b85a02b44c247cf9ef9f5ad0999bf8ad85f
plattfot <fredriks@d2.com>
2017-02-06 14:37:25 -0800

    Fixed context file for openvdb_houdini

--- openvdb_houdini/context.py
+++ openvdb_houdini/context.py
@@ -12,49 +12,32 @@ class Openvdb_houdini(Context):
         self.environ['OPENVDB_HOUDINI_PACKAGE_ROOT'] = self.package_root
         self.environ['OPENVDB_HOUDINI_VERSION'] = self.version
 
-        # DD_HOUD_MAJ_VERSION -> $HOUDINI_MAJOR_RELEASE.$HOUDINI_MINIR_RELEASE
-        
-        self.environ['OPENVDB_HOUDINI_INCLUDE_PATH'] = self.expandPaths(
-            self.package_root + '/houdini/$DD_HOUD_MAJ_VERSION/include'
-        )
-
-        self.environ['OPENVDB_HOUDINI_LIBRARY_PATH'] = self.expandPaths(
-            self.package_root + '/houdini/$DD_HOUD_MAJ_VERSION/lib'
+        path = self.expandPaths(
+            "$OPENVDB_HOUDINI_PACKAGE_ROOT/houdini/$HOUDINI_VERSION"
         )
 
         # Fallback to the major release if openvdb_houdini isn't built
         # for the specific version.
-        self.environ[ 'LD_LIBRARY_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$DD_HOUD_MAJ_VERSION/lib',
-            '$LD_LIBRARY_PATH'
-        )
-            
-        self.environ[ 'LD_LIBRARY_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$HOUDINI_VERSION/lib',
-            '$LD_LIBRARY_PATH'
-        )
-
-        # Script path
-        self.environ[ 'HOUDINI_SCRIPT_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$DD_HOUD_MAJ_VERSION/scripts',
-            '$HOUDINI_SCRIPT_PATH',
-        )
+        if not path:
+            # DD_HOUD_MAJ_VERSION -> $HOUDINI_MAJOR_RELEASE.$HOUDINI_MINIR_RELEASE
+            path = self.expandPaths(
+                '$OPENVDB_HOUDINI_PACKAGE_ROOT/houdini/$DD_HOUD_MAJ_VERSION'
+            )
+        
+        self.environ['OPENVDB_HOUDINI_INCLUDE_PATH'] = '%/include'%path
+        self.environ['OPENVDB_HOUDINI_LIBRARY_PATH'] = '%/lib'%path
 
         self.environ[ 'HOUDINI_SCRIPT_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$HOUDINI_VERSION/scripts',
-            '$HOUDINI_SCRIPT_PATH',
+            '%/scripts'%path,
+            '$HOUDINI_SCRIPT_PATH'
         )
-
-        # DSO path
+        
         self.environ[ 'HOUDINI_DSO_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$DD_HOUD_MAJ_VERSION/dso',
-            '$HOUDINI_DSO_PATH',
+            '%/dso'%path,
+            '$HOUDINI_DSO_PATH'
         )
-
-        self.environ[ 'HOUDINI_DSO_PATH' ] = self.expandPaths(
-            self.package_root + '/houdini/$HOUDINI_VERSION/dso',
-            '$HOUDINI_DSO_PATH',
+        
+        self.environ[ 'LD_LIBRARY_PATH' ] = self.expandPaths(
+            '$OPENVDB_HOUDINI_LIBRARY_PATH', 
+            '$LD_LIBRARY_PATH'
         )
-
-
-
