f7e81be1870ee299000017fab318754eb9bb7fb4
plattfot <fredriks@d2.com>
2017-04-07 18:14:34 -0700

    Ticket #94833: Switched to using findNearestBuild to get the fallback path

--- openvdb_houdini/context.py
+++ openvdb_houdini/context.py
@@ -1,5 +1,6 @@
 
 from dd.runtime import Context
+from houdini_plugin_util import findNearestBuild
 
 
 class Openvdb_houdini(Context):
@@ -11,33 +12,26 @@ class Openvdb_houdini(Context):
     def setupEnvironment(self):
         self.environ['OPENVDB_HOUDINI_PACKAGE_ROOT'] = self.package_root
         self.environ['OPENVDB_HOUDINI_VERSION'] = self.version
+        
+        path = findNearestBuild( self.expandPaths('$OPENVDB_HOUDINI_PACKAGE_ROOT/houdini'),
+                                 self.environ['HOUDINI_VERSION'])
 
-        path = self.expandPaths(
-            "$OPENVDB_HOUDINI_PACKAGE_ROOT/houdini/$HOUDINI_VERSION"
-        )
+        if path:
+        
+            self.environ['OPENVDB_HOUDINI_INCLUDE_PATH'] = '%s/include'%path
+            self.environ['OPENVDB_HOUDINI_LIBRARY_PATH'] = '%s/lib'%path
 
-        # Fallback to the major release if openvdb_houdini isn't built
-        # for the specific version.
-        if not path:
-            # DD_HOUD_MAJ_VERSION -> $HOUDINI_MAJOR_RELEASE.$HOUDINI_MINIR_RELEASE
-            path = self.expandPaths(
-                '$OPENVDB_HOUDINI_PACKAGE_ROOT/houdini/$DD_HOUD_MAJ_VERSION'
+            self.environ[ 'HOUDINI_SCRIPT_PATH' ] = self.expandPaths(
+                '%s/scripts'%path,
+                '$HOUDINI_SCRIPT_PATH'
             )
         
-        self.environ['OPENVDB_HOUDINI_INCLUDE_PATH'] = '%s/include'%path
-        self.environ['OPENVDB_HOUDINI_LIBRARY_PATH'] = '%s/lib'%path
-
-        self.environ[ 'HOUDINI_SCRIPT_PATH' ] = self.expandPaths(
-            '%s/scripts'%path,
-            '$HOUDINI_SCRIPT_PATH'
-        )
+            self.environ[ 'HOUDINI_DSO_PATH' ] = self.expandPaths(
+                '%s/dso'%path,
+                '$HOUDINI_DSO_PATH'
+            )
         
-        self.environ[ 'HOUDINI_DSO_PATH' ] = self.expandPaths(
-            '%s/dso'%path,
-            '$HOUDINI_DSO_PATH'
-        )
-        
-        self.environ[ 'LD_LIBRARY_PATH' ] = self.expandPaths(
-            '$OPENVDB_HOUDINI_LIBRARY_PATH', 
-            '$LD_LIBRARY_PATH'
-        )
+            self.environ[ 'LD_LIBRARY_PATH' ] = self.expandPaths(
+                '$OPENVDB_HOUDINI_LIBRARY_PATH', 
+                '$LD_LIBRARY_PATH'
+            )
