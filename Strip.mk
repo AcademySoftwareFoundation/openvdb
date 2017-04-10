d087df3b3a0bcbdf0862f73335acc40da283eebd
plattfot <fredriks@d2.com>
2017-04-10 16:53:19 -0700

    Removed files used by pb2, cleaned up the makefiles, parsing version from manifest when possible.

--- Strip.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Strip rpaths
-.PHONY: strip-rpath-%
-strip-rpath-%:
-	@echo "stripping rpath from libs for version $*..."
-	@cd $(STRIP_PATH)/$* && \
-	find -name "*.so*" -exec patchelf --set-rpath '' {} \;
--- if_houdini_ge_15.mk
+++
@@ -1,8 +0,0 @@
-# Check if houdini version is greater than or equal to 15
-hou_ge_15 := $(shell test $(HOUDINI_MAJOR_RELEASE) -ge 15 && echo 'true')
-ifeq ($(hou_ge_15),true)
-  GCC_HOME:=/tools/package/gcc/$(GCC_VERSION)
-  CXX:=$(GCC_HOME)/bin/g++
-  CXXFLAGS += -std=c++11
-  using_newer_gcc := yes
-endif
