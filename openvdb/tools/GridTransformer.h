88ae8db79dd645dac7bec282b48e58d14b8c9172
Peter Cucka <peter.cucka@dreamworks.com>
2019-02-21 16:05:47 -0800

    Additional improvements to tools::local_util::decompose()
    - Change return value from bool to invalid/valid/unique enum.
    - Map angles to [0, 2π] for comparison.
    
    Signed-off-by: Peter Cucka <peter.cucka@dreamworks.com>

--- openvdb/tools/GridTransformer.h
+++ openvdb/tools/GridTransformer.h
@@ -294,15 +294,24 @@ private:
 
 namespace local_util {
 
+enum { DECOMP_INVALID = 0, DECOMP_VALID = 1, DECOMP_UNIQUE = 2 };
+
 /// @brief Decompose an affine transform into scale, rotation (XYZ order),
 /// and translation components.
-/// @return @c false if the given matrix is not affine or cannot otherwise be decomposed.
+/// @return DECOMP_INVALID if the given matrix is not affine or cannot
+/// be decomposed, DECOMP_UNIQUE if the matrix has a unique decomposition,
+/// DECOMP_VALID otherwise
 template<typename T>
-inline bool
+inline int
 decompose(const math::Mat4<T>& m, math::Vec3<T>& scale,
     math::Vec3<T>& rotate, math::Vec3<T>& translate)
 {
-    if (!math::isAffine(m)) return false;
+    if (!math::isAffine(m)) return DECOMP_INVALID;
+
+    const auto posAngle = [](T angle) {
+        while (angle < T(0)) { angle += M_PI * 2.0; }
+        return angle;
+    };
 
     // This is the translation in world space
     translate = m.getTranslation();
@@ -321,11 +330,9 @@ decompose(const math::Mat4<T>& m, math::Vec3<T>& scale,
 
     T minAngle = std::numeric_limits<T>::max();
 
-    // If the transformation matrix contains a reflection,
-    // test different negative scales to find a decomposition
-    // that favors the optimal resampling algorithm.
+    // If the transformation matrix contains a reflection, test different negative scales
+    // to find a decomposition that favors the optimal resampling algorithm.
     for (size_t n = 0; n < 8; ++n) {
-
         const math::Vec3<T> signedScale(
             n & 0x1 ? -unsignedScale.x() : unsignedScale.x(),
             n & 0x2 ? -unsignedScale.y() : unsignedScale.y(),
@@ -344,9 +351,8 @@ decompose(const math::Mat4<T>& m, math::Vec3<T>& scale,
             math::scale<math::Mat3<T> >(signedScale);
 
         if (xform.eq(rebuild)) {
-
-            const T maxAngle = std::max(std::abs(tmpAngle[0]),
-                std::max(std::abs(tmpAngle[1]), std::abs(tmpAngle[2])));
+            const T maxAngle = math::Max(
+                posAngle(tmpAngle[0]), posAngle(tmpAngle[1]), posAngle(tmpAngle[2]));
 
             if (!(minAngle < maxAngle)) { // Update if less or equal.
 
@@ -365,13 +371,15 @@ decompose(const math::Mat4<T>& m, math::Vec3<T>& scale,
         }
     }
 
-    if (!validDecomposition || (hasRotation && !hasUniformScale)) {
+    if (!validDecomposition) {
         // The decomposition is invalid if the transformation matrix contains shear.
-        // No unique decomposition if scale is nonuniform and rotation is nonzero.
-        return false;
+        return DECOMP_INVALID;
     }
-
-    return true;
+    if (hasRotation && !hasUniformScale) {
+        // No unique decomposition if scale is nonuniform and rotation is nonzero.
+        return DECOMP_VALID;
+    }
+    return DECOMP_UNIQUE;
 }
 
 } // namespace local_util
