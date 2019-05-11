8f12e3c2e0232456eff06642e1620a9538811f29
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2019-05-11 14:27:50 +0100

    Fixes for Python3 and loading the python module on MacOS
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- openvdb/python/pyOpenVDBModule.cc
+++ openvdb/python/pyOpenVDBModule.cc
@@ -320,7 +320,11 @@ struct PointIndexConverter
     /// @return nullptr if the given Python object is not convertible to the PointIndex.
     static void* convertible(PyObject* obj)
     {
+#if PY_MAJOR_VERSION >= 3
+        if (!PyLong_Check(obj)) return nullptr; // not a Python integer
+#else
         if (!PyInt_Check(obj)) return nullptr; // not a Python integer
+#endif
         return obj;
     }
 
@@ -336,8 +340,11 @@ struct PointIndexConverter
 
         // Extract the PointIndex from the python integer
         PointIndexT* index = static_cast<PointIndexT*>(storage);
-
+#if PY_MAJOR_VERSION >= 3
+        *index = static_cast<IntType>(PyLong_AsLong(obj));
+#else
         *index = static_cast<IntType>(PyInt_AsLong(obj));
+#endif
     }
 
     /// Register both the PointIndex-to-integer and the integer-to-PointIndex converters.
