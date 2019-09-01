5e7d45d802d8b1d5320b28ec297e4c02ded4827d
Dan Bailey <danbailey@ilm.com>
2019-09-01 11:10:43 -0700

    Add determinism test to Point Partitioner unit test
    Signed-off-by: Dan Bailey <danbailey@ilm.com>

--- openvdb/unittest/TestPointPartitioner.cc
+++ openvdb/unittest/TestPointPartitioner.cc
@@ -4,6 +4,8 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <openvdb/tools/PointPartitioner.h>
 
+#include <openvdb/math/Math.h> // math::Rand01
+
 #include <vector>
 
 
@@ -12,9 +14,11 @@ class TestPointPartitioner: public CppUnit::TestCase
 public:
     CPPUNIT_TEST_SUITE(TestPointPartitioner);
     CPPUNIT_TEST(testPartitioner);
+    CPPUNIT_TEST(testDeterminism);
     CPPUNIT_TEST_SUITE_END();
 
     void testPartitioner();
+    void testDeterminism();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(TestPointPartitioner);
@@ -96,3 +100,62 @@ TestPointPartitioner::testPartitioner()
     }
 }
 
+void
+TestPointPartitioner::testDeterminism()
+{
+    const size_t pointCount = 1000*1000;
+    const float voxelSize = 0.1f;
+
+    // create a box of points positioned randomly between -1.0 and 1.0
+
+    openvdb::math::Rand01<float> rand01(0);
+
+    std::vector<openvdb::Vec3s> points(pointCount, openvdb::Vec3s(0.f));
+    for (size_t n = 0; n < pointCount; ++n) {
+        points[n].x() = (rand01() * 2.0f) - 1.0f;
+        points[n].y() = (rand01() * 2.0f) - 1.0f;
+        points[n].z() = (rand01() * 2.0f) - 1.0f;
+    }
+
+    PointList pointList(points);
+
+    const openvdb::math::Transform::Ptr transform =
+            openvdb::math::Transform::createLinearTransform(voxelSize);
+
+    typedef openvdb::tools::UInt32PointPartitioner PointPartitioner;
+
+    std::vector<PointPartitioner::Ptr> partitioners;
+
+    // create 10 point partitioners
+
+    for (size_t i = 0; i < 10; i++) {
+        partitioners.push_back(PointPartitioner::create(pointList, *transform));
+    }
+
+    // verify their sizes are the same
+
+    for (size_t i = 1; i < partitioners.size(); i++) {
+        CPPUNIT_ASSERT_EQUAL(partitioners[0]->size(), partitioners[i]->size());
+    }
+
+    // verify their contents are the same
+
+    for (size_t i = 1; i < partitioners.size(); i++) {
+        for (size_t n = 1; n < partitioners[0]->size(); n++) {
+            CPPUNIT_ASSERT_EQUAL(partitioners[0]->origin(n), partitioners[i]->origin(n));
+
+            auto it1 = partitioners[0]->indices(n);
+            auto it2 = partitioners[i]->indices(n);
+
+            size_t bucketSize = it1.size();
+            CPPUNIT_ASSERT_EQUAL(bucketSize, it2.size());
+
+            for (size_t j = 0; j < bucketSize; j++) {
+                CPPUNIT_ASSERT_EQUAL(*it1, *it2);
+
+                ++it1;
+                ++it2;
+            }
+        }
+    }
+}
