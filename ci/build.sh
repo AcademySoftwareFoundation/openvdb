891c376253c960becd538b71172b8f5bf71f506e
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2020-04-17 19:17:54 +0100

    Added a basic MacOS CI build and test
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,18 @@
 
 set -ex
 
-COMPILER="$1"
-RELEASE="$2"
-ABI="$3"
-BLOSC="$4"
-SIMD="$5"
+COMPILER="$1"; shift
+RELEASE="$1"; shift
+ABI="$1"; shift
+BLOSC="$1"; shift
+SIMD="$1"; shift
+CMAKE_EXTRA="$@"
+
+CXX_STRICT=ON
+if [ $(uname -s) == "Darwin" ]; then
+    echo "Disabling compiler warnings on MacOS"
+    CXX_STRICT=OFF
+fi
 
 mkdir build
 cd build
@@ -16,7 +23,7 @@ cmake \
     -DOPENVDB_ABI_VERSION_NUMBER=${ABI} \
     -DOPENVDB_USE_DEPRECATED_ABI=ON \
     -DUSE_BLOSC=${BLOSC} \
-    -DOPENVDB_CXX_STRICT=ON \
+    -DOPENVDB_CXX_STRICT=${CXX_STRICT} \
     -DOPENVDB_BUILD_PYTHON_MODULE=ON \
     -DOPENVDB_BUILD_UNITTESTS=ON \
     -DOPENVDB_BUILD_BINARIES=ON \
@@ -25,5 +32,6 @@ cmake \
     -DOPENVDB_BUILD_VDB_RENDER=ON \
     -DOPENVDB_BUILD_VDB_VIEW=ON \
     -DOPENVDB_SIMD=${SIMD} \
+    ${CMAKE_EXTRA} \
     ..
 make -j2
