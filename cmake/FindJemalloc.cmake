733a553fd3f418f9fe62646c078366f79df87f52
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2020-04-23 17:44:49 +0100

    Various improvements to CMake find modules  - Removing unnecessary DLL methods  - Adding better lib type detection on UNIX  - Added GNU path support  - Added required defined for static TBB on Windows  - Added required defined for static IlmBase and OpenEXR on Windows  - Improved handling of finding static builds of OpenVDB
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- cmake/FindJemalloc.cmake
+++ cmake/FindJemalloc.cmake
@@ -65,6 +65,7 @@ may be provided to tell this module where to look.
 #]=======================================================================]
 
 cmake_minimum_required(VERSION 3.3)
+include(GNUInstallDirs)
 
 # Monitoring <PackageName>_ROOT variables
 if(POLICY CMP0074)
@@ -118,29 +119,24 @@ list(APPEND _JEMALLOC_LIBRARYDIR_SEARCH_DIRS
 set(_JEMALLOC_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
 
 if(WIN32)
-  list(APPEND CMAKE_FIND_LIBRARY_SUFFIXES
-    "_dll.lib"
-  )
-elseif(UNIX)
   if(JEMALLOC_USE_STATIC_LIBS)
-    list(APPEND CMAKE_FIND_LIBRARY_SUFFIXES
-      ".a"
-    )
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib")
+  else()
+    list(APPEND CMAKE_FIND_LIBRARY_SUFFIXES "_dll.lib")
+  endif()
+else()
+  if(JEMALLOC_USE_STATIC_LIBS)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
   endif()
 endif()
 
 # Build suffix directories
 
 set(JEMALLOC_PATH_SUFFIXES
+  ${CMAKE_INSTALL_LIBDIR}
   lib64
   lib
 )
-
-# platform branching
-
-if(UNIX)
-  list(INSERT JEMALLOC_PATH_SUFFIXES 0 lib/x86_64-linux-gnu)
-endif()
 
 find_library(Jemalloc_LIBRARY jemalloc
   ${_FIND_JEMALLOC_ADDITIONAL_OPTIONS}
@@ -166,16 +162,31 @@ find_package_handle_standard_args(Jemalloc
 )
 
 if(Jemalloc_FOUND)
+  # Configure lib type. If XXX_USE_STATIC_LIBS, we always assume a static
+  # lib is in use. If win32, we can't mark the import .libs as shared, so
+  # these are always marked as UNKNOWN. Otherwise, infer from extension.
+  set(JEMALLOC_LIB_TYPE UNKNOWN)
+  if(JEMALLOC_USE_STATIC_LIBS)
+    set(JEMALLOC_LIB_TYPE STATIC)
+  elseif(UNIX)
+    get_filename_component(_JEMALLOC_EXT ${Jemalloc_LIBRARY} EXT)
+    if(_JEMALLOC_EXT STREQUAL ".a")
+      set(JEMALLOC_LIB_TYPE STATIC)
+    elseif(_JEMALLOC_EXT STREQUAL ".so" OR
+           _JEMALLOC_EXT STREQUAL ".dylib")
+      set(JEMALLOC_LIB_TYPE SHARED)
+    endif()
+  endif()
+
   set(Jemalloc_LIBRARIES ${Jemalloc_LIBRARY})
-  set(Jemalloc_DEFINITIONS ${PC_Jemalloc_CFLAGS_OTHER})
 
   get_filename_component(Jemalloc_LIBRARY_DIRS ${Jemalloc_LIBRARY} DIRECTORY)
 
   if(NOT TARGET Jemalloc::jemalloc)
-    add_library(Jemalloc::jemalloc UNKNOWN IMPORTED)
+    add_library(Jemalloc::jemalloc ${JEMALLOC_LIB_TYPE} IMPORTED)
     set_target_properties(Jemalloc::jemalloc PROPERTIES
       IMPORTED_LOCATION "${Jemalloc_LIBRARIES}"
-      INTERFACE_COMPILE_DEFINITIONS "${Jemalloc_DEFINITIONS}"
+      INTERFACE_COMPILE_OPTIONS "${PC_Jemalloc_CFLAGS_OTHER}"
     )
   endif()
 elseif(Jemalloc_FIND_REQUIRED)
