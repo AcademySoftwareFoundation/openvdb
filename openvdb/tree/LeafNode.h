7397d74038e42d6c944837ae6e1e9436ba09a269
Megidd Git <megiddgit@gmail.com>
2020-08-17 11:43:28 +0430

    Add declaration and implementation to leaf
    Signed-off-by: Megidd Git <megiddgit@gmail.com>

--- openvdb/tree/LeafNode.h
+++ openvdb/tree/LeafNode.h
@@ -131,6 +131,8 @@ public:
     static Index32 leafCount() { return 1; }
     /// no-op
     void nodeCount(std::vector<Index32> &) const {}
+    /// no-op
+    void activeTileCountByLevel(std::vector<Index32>&) const {}
     /// Return the non-leaf count for this node, which is zero.
     static Index32 nonLeafCount() { return 0; }
     /// Return the child count for this node, which is zero.
