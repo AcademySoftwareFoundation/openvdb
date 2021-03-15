0df03eea05f6d424ebcbb07ee334f1e1505f80a9
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2021-03-15 14:30:11 +0000

    Updated AX example docs
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- ci/extract_test_examples.sh
+++ ci/extract_test_examples.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+#################################################################################
+# This script extracts all code blocks from AX documentation which are NOT      #
+# marked as cpp/sh/unparsed and attempts to parse or compile them through the   #
+# vdb_ax command line binary. Code blocks can be marked with a preceding        #
+# notation to determine compilation mode:                                       #
+#    <!--- P --->  = points                                                     #
+#    <!--- V --->  = volumes                                                    #
+#    <!--- A --->  = all                                                        #
+# If not marked, code is only parsed (though ideally all code blocks should be  #
+# compiled)                                                                     #
+#################################################################################
 set -e
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
@@ -11,6 +22,8 @@ DOCS+=($ROOT/doc/ax/axexamples.txt)
 # DOCS+=($ROOT/doc/ax/axfunctionlist.txt) do not test this file
 DOCS+=($ROOT/doc/ax/doc.txt)
 
+uncompiled=0
+
 for DOC in "${DOCS[@]}"; do
     echo "Checking doxygen code in '$DOC...'"
     # Make sure the file exists
@@ -20,9 +33,10 @@ for DOC in "${DOCS[@]}"; do
     fi
 
     # Extract all code segments from doxygen documentation in between @code and @endcode
-    data=$(sed -n '/^ *@code.* *$/,/^ *@endcode *$/p' $DOC)
+    data=$(sed -n '/@code.* *$/,/^ *@endcode *$/p' $DOC)
 
     str=""
+    compile=""
     skip=false
     count=0
     # For each extracted line, rebuild each code segment from @code and @endcode and
@@ -37,6 +51,16 @@ for DOC in "${DOCS[@]}"; do
             skip=true
         elif [[ $line == @code* ]]; then
             str=""
+            compile="none"
+        elif [[ $line == "<!--- V --->@code"* ]]; then
+            str=""
+            compile="volumes"
+        elif [[ $line == "<!--- P --->@code"* ]]; then
+            str=""
+            compile="points"
+        elif [[ $line == "<!--- A --->@code"* ]]; then
+            str=""
+            compile=""
         elif [[ $line == @endcode ]]; then
             echo -e "\nTesting [$count]:"
             if [ "$skip" = true ]; then
@@ -44,14 +68,25 @@ for DOC in "${DOCS[@]}"; do
                 echo -e "$str" | sed 's/^/    /'
             else
                 # parse
-                $VDB_AX analyze -s "$str" -v
+                if [ "$compile" = "none" ]; then
+                    $VDB_AX analyze -s "$str" -v
+                    uncompiled=$((uncompiled+1))
+                else
+                    $VDB_AX analyze -s "$str" --try-compile $compile -v
+                fi
             fi
             skip=false
             count=$((count+1))
             str=""
+            compile="none"
         else
             str+="$line"$'\n'
         fi
-
     done <<< "$data"
 done
+
+echo ""
+echo "Extract test examples completed successfully"
+if [ $uncompiled -gt 0 ]; then
+    echo " with $uncompiled uncompiled tests"
+fi
