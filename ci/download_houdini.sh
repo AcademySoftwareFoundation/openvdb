9cbad8c4303d26b9333c5a3d71b2732552ea1819
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2021-08-15 17:47:13 +0100

    Added exr libs to the Houdini Cache for VDB render and bump the cache version
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- ci/download_houdini.sh
+++ ci/download_houdini.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 HOUDINI_MAJOR="$1"
 GOLD="$2"
@@ -35,6 +35,10 @@ cp -r dsolib/libtbb* ../hou/dsolib/.
 cp -r dsolib/libHalf* ../hou/dsolib/.
 cp -r dsolib/libjemalloc* ../hou/dsolib/.
 cp -r dsolib/liblzma* ../hou/dsolib/.
+cp -r dsolib/libIex* ../hou/dsolib/.
+cp -r dsolib/libImath* ../hou/dsolib/.
+cp -r dsolib/libIlmThread* ../hou/dsolib/.
+cp -r dsolib/libIlmImf* ../hou/dsolib/.
 
 # needed for < H18.0 (due to sesitag)
 if [ "$HOUDINI_MAJOR" == "17.0" ] || [ "$HOUDINI_MAJOR" == "17.5" ]; then
