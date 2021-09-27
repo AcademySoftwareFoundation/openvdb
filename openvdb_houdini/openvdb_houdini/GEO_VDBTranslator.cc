feea85a68c0471a6f3fd3eaec67098b91c2563c3
jlait <jlait@andorra.sidefx.com>
2021-09-27 09:49:17 -0400

    Re-tab to match VDB standards
    Signed-off-by: jlait <jlait@andorra.sidefx.com>

--- openvdb_houdini/openvdb_houdini/GEO_VDBTranslator.cc
+++ openvdb_houdini/openvdb_houdini/GEO_VDBTranslator.cc
@@ -132,7 +132,7 @@ GEO_VDBTranslator::fileStat(const char *filename, GA_Stat &stat, uint /*level*/)
             meta_maxbbox = stats->getMetadata<openvdb::Vec3IMetadata>("file_bbox_max");
             // empty vdbs have invalid bounding boxes, and often very
             // huge ones, so we intentionally skip them here.
-	    if (meta_minbbox && meta_maxbbox &&
+            if (meta_minbbox && meta_maxbbox &&
                 meta_minbbox->value().x() <= meta_maxbbox->value().x() &&
                 meta_minbbox->value().y() <= meta_maxbbox->value().y() &&
                 meta_minbbox->value().z() <= meta_maxbbox->value().z()
