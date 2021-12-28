bb657ef2f30a99f7b3593da8745ea1e5fe815161
3Descape <miclack30@gmail.com>
2021-12-28 17:26:49 +0100

    Fix windows compilation issues
    Current problem: When using the most recent version of oneTBB on windows with MSVC(2019), linking results in an LNK1104 "cannot open file tbb12.lib" error.
    Cause: the target e.g. openvdb_shared references tbb.lib. This on the other hand is just a reference to tbb12.lib, but the directory containing that file is not included in additional library directories. Therefore the linker can't find it.
    This pull request includes the directory into the additional link directories and therefore lets the linker find the missing library files.
    
    Signed-off-by: Michael Lackner <mlackner@student.tugraz.at>

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -418,6 +418,8 @@ foreach(COMPONENT ${TBB_FIND_COMPONENTS})
     set_target_properties(TBB::${COMPONENT} PROPERTIES
       IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
       IMPORTED_LOCATION "${Tbb_${COMPONENT}_LIBRARY}")
+      
+    target_link_directories(TBB::${COMPONENT} INTERFACE "${Tbb_LIBRARY_DIRS}")
 
     # Release location
     if(EXISTS "${Tbb_${COMPONENT}_LIBRARY_RELEASE}")
