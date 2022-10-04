b844a66ef6bbfc27e544e164fd27b960a996e71e
ghurstunither <62885595+ghurstunither@users.noreply.github.com>
2022-10-04 13:58:11 -0400

    Update BuildSettings.m
    Updates build settings for windows. User must specify the location of vcpkg with $vcpkgDir.
    
    Signed-off-by: ghurstunither <62885595+ghurstunither@users.noreply.github.com>

--- openvdb_wolfram/OpenVDBLink/BuildSettings.m
+++ openvdb_wolfram/OpenVDBLink/BuildSettings.m
@@ -3,38 +3,31 @@
 Switch[$OperatingSystem,
     "MacOSX",
         $buildSettings = {
-            "CompileOptions" -> {"-std=c++14 -ltbb -lHalf -lopenvdb -flto -L/usr/local/opt/ilmbase/lib -I/usr/local/opt/ilmbase/include"},
+            "CompileOptions" -> {"-std=c++14 -ltbb -lHalf -lopenvdb -flto"},
             "Compiler" -> CCompilerDriver`ClangCompiler`ClangCompiler
         };
         $libraryName = "OpenVDBLink.dylib",
     "Windows",
+        $vcpkgDir = $HomeDirectory; (* change this to the location of vcpkg *)
+        $vcpkgInstalled = FileNameJoin[{$vcpkgDir, "vcpkg", "installed", "x64-windows"}];
+        $vcpkgLib = FileNameJoin[{$vcpkgInstalled, "lib"}];
+        
         $buildSettings = {
-            "CompileOptions" -> {"/bigobj /EHsc /showIncludes /FC /diagnostics:classic "},
+            "CompileOptions" -> {"/EHsc", "/GL", "/wd4244", "/DNOMINMAX"},
+            "Compiler" -> CCompilerDriver`VisualStudioCompiler`VisualStudioCompiler,
             "IncludeDirectories" -> {
-                (* these might be avoidable with proper path variables? *)
-                "C:\\Program Files\\OpenVDB\\include",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\include",
-                "C:\\src\\openvdb\\include",
-                "C:\\src\\vcpkg\\packages",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\bin",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\include",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\include\\boost",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\include\\boost\\type_traits",
-                "C:\\src\\vcpkg\\packages\\boost-numeric-conversion_x64-windows\\include",
-                "C:\\src\\vcpkg\\packages\\boost-numeric-conversion_x64-windows\\include\\boost"
+                FileNameJoin[{"C:", "Program Files", "OpenVDB", "include"}],
+                FileNameJoin[{$vcpkgInstalled, "include"}],
+                FileNameJoin[{$vcpkgDir, "vcpkg", "packages", "boost-numeric-conversion_x64-windows", "include"}]
             },
-            "ExtraObjectFiles" -> {
-                "C:\\src\\openvdb\\build\\openvdb\\openvdb\\Release\\libopenvdb.lib",
-                "C:\\src\\openvdb\\build\\openvdb\\openvdb\\Release\\openvdb.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\blosc.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\snappy.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\tbbmalloc_proxy.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\tbb.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\tbbmalloc.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\zlib.lib",
-                "C:\\src\\vcpkg\\installed\\x64-windows\\lib\\zstd.lib"
-            }
+            "ExtraObjectFiles" -> Join[
+                {
+                    FileNameJoin[{$vcpkgDir, "openvdb", "build", "openvdb", "openvdb", "Release", "libopenvdb.lib"}],
+                    FileNameJoin[{$vcpkgLib, "blosc.lib"}],
+                    FileNameJoin[{$vcpkgLib, "zlib.lib"}]
+                },
+                FileNames[FileNameJoin[{$vcpkgLib, "tbb*.lib"}]]
+            ]
         };
         $libraryName = "OpenVDBLink.dll",
     _, (* TODO "Unix", etc. *)
