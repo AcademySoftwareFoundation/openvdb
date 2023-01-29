dreamworksanimation/openvdb_dev

Removed boost/string methods in favour of utility methods in util/Name.h
Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

openvdb/Grid.cc
@@ -4,8 +4,7 @@
 #include "Grid.h"
 
 #include <openvdb/Metadata.h>
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/algorithm/string/trim.hpp>
+#include <openvdb/util/Name.h>
 #include <tbb/mutex.h>
 
 
@@ -127,8 +126,8 @@ GridBase::stringToGridClass(const std::string& s)
 {
     GridClass ret = GRID_UNKNOWN;
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::util::trim(str);
+    openvdb::util::to_lower(str);
     if (str == gridClassToString(GRID_LEVEL_SET)) {
         ret = GRID_LEVEL_SET;
     } else if (str == gridClassToString(GRID_FOG_VOLUME)) {
@@ -201,8 +200,8 @@ GridBase::stringToVecType(const std::string& s)
 {
     VecType ret = VEC_INVARIANT;
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::util::trim(str);
+    openvdb::util::to_lower(str);
     if (str == vecTypeToString(VEC_COVARIANT)) {
         ret = VEC_COVARIANT;
     } else if (str == vecTypeToString(VEC_COVARIANT_NORMALIZE)) {

openvdb/io/GridDescriptor.cc
@@ -4,8 +4,7 @@
 #include "GridDescriptor.h"
 
 #include <openvdb/Exceptions.h>
-#include <boost/algorithm/string/predicate.hpp> // for boost::ends_with()
-#include <boost/algorithm/string/erase.hpp> // for boost::erase_last()
+#include <openvdb/util/Name.h>
 #include <sstream>
 
 
@@ -79,9 +78,9 @@ GridDescriptor::read(std::istream &is)
 
     // Read in the grid type.
     mGridType = readString(is);
-    if (boost::ends_with(mGridType, HALF_FLOAT_TYPENAME_SUFFIX)) {
+    if (openvdb::util::ends_with(mGridType, HALF_FLOAT_TYPENAME_SUFFIX)) {
         mSaveFloatAsHalf = true;
-        boost::erase_last(mGridType, HALF_FLOAT_TYPENAME_SUFFIX);
+        mGridType.resize(mGridType.size() - std::strlen(HALF_FLOAT_TYPENAME_SUFFIX));
     }
 
     if (getFormatVersion(is) >= OPENVDB_FILE_VERSION_GRID_INSTANCING) {

openvdb/math/FiniteDifference.h
@@ -7,12 +7,11 @@
 #define OPENVDB_MATH_FINITEDIFFERENCE_HAS_BEEN_INCLUDED
 
 #include <openvdb/Types.h>
+#include <openvdb/util/Name.h>
 #include "Math.h"
 #include "Coord.h"
 #include "Vec3.h"
 #include <string>
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/algorithm/string/trim.hpp>
 
 #ifdef DWA_OPENVDB
 #include <simd/Simd.h>
@@ -80,8 +79,8 @@ stringToDScheme(const std::string& s)
     DScheme ret = UNKNOWN_DS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::util::trim(str);
+    openvdb::util::to_lower(str);
 
     if (str == dsSchemeToString(CD_2NDT)) {
         ret = CD_2NDT;
@@ -194,8 +193,8 @@ stringToBiasedGradientScheme(const std::string& s)
     BiasedGradientScheme ret = UNKNOWN_BIAS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::util::trim(str);
+    openvdb::util::to_lower(str);
 
     if (str == biasedGradientSchemeToString(FIRST_BIAS)) {
         ret = FIRST_BIAS;
@@ -259,8 +258,8 @@ stringToTemporalIntegrationScheme(const std::string& s)
     TemporalIntegrationScheme ret = UNKNOWN_TIS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::util::trim(str);
+    openvdb::util::to_lower(str);
 
     if (str == temporalIntegrationSchemeToString(TVD_RK1)) {
         ret = TVD_RK1;

openvdb/util/Name.h
@@ -9,6 +9,8 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
 namespace openvdb {
 OPENVDB_USE_VERSION_NAMESPACE
@@ -34,6 +36,77 @@ writeString(std::ostream& os, const Name& name)
     os.write(reinterpret_cast<char*>(&size), sizeof(uint32_t));
     os.write(&name[0], size);
 }
+
+namespace util {
+
+template <typename ContainerT>
+inline void split(ContainerT& out, const std::string& in, const char delim)
+{
+    out.clear();
+    if (in.empty()) return;
+    std::string::size_type pos = 0u, old = 0u;
+    while ((pos = in.find(delim, pos)) != std::string::npos) {
+        out.insert(out.end(), in.substr(old, (pos-old)));
+        old = ++pos;
+    }
+    // last element
+    out.insert(out.end(), in.substr(old, in.length()-1));
+}
+
+template <typename ContainerT>
+inline void split(ContainerT& out, const std::string& in, const std::set<char>& delims)
+{
+    out.clear();
+    if (in.empty()) return;
+    std::string::size_type pos = 0u, old = 0u;
+    const std::string seq(delims.begin(), delims.end());
+    while ((pos = in.find_first_of(seq, pos)) != std::string::npos) {
+        out.insert(out.end(), in.substr(old, (pos-old)));
+        old = ++pos;
+    }
+    // last element
+    out.insert(out.end(), in.substr(old, in.length()-1));
+}
+
+inline bool starts_with(const std::string& in, const std::string& start)
+{
+    if (start.length() > in.length()) return false;
+    return std::equal(start.begin(), start.end(), in.begin());
+}
+
+inline bool ends_with(const std::string& in, const std::string& end)
+{
+    if (end.length() > in.length()) return false;
+    return std::equal(end.rbegin(), end.rend(), in.rbegin());
+}
+
+inline void ltrim(std::string& s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+inline void rtrim(std::string& s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+inline void trim(std::string& s)
+{
+    ltrim(s);
+    rtrim(s);
+}
+
+inline void to_lower(std::string& s)
+{
+    std::transform(s.begin(), s.end(), s.begin(),
+        [](auto c){ return std::tolower(c); });
+}
+
+} // util
 
 } // namespace OPENVDB_VERSION_NAME
 } // namespace openvdb

AcademySoftwareFoundation/openvdb

openvdb/openvdb/Grid.cc
@@ -2,10 +2,8 @@
 // SPDX-License-Identifier: MPL-2.0
 
 #include "Grid.h"
-
 #include "Metadata.h"
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/algorithm/string/trim.hpp>
+#include "util/Name.h"
 #include <mutex>
 
 
@@ -124,8 +122,8 @@ GridBase::stringToGridClass(const std::string& s)
 {
     GridClass ret = GRID_UNKNOWN;
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::string::trim(str);
+    openvdb::string::to_lower(str);
     if (str == gridClassToString(GRID_LEVEL_SET)) {
         ret = GRID_LEVEL_SET;
     } else if (str == gridClassToString(GRID_FOG_VOLUME)) {
@@ -198,8 +196,8 @@ GridBase::stringToVecType(const std::string& s)
 {
     VecType ret = VEC_INVARIANT;
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::string::trim(str);
+    openvdb::string::to_lower(str);
     if (str == vecTypeToString(VEC_COVARIANT)) {
         ret = VEC_COVARIANT;
     } else if (str == vecTypeToString(VEC_COVARIANT_NORMALIZE)) {

openvdb/openvdb/io/GridDescriptor.cc
@@ -4,8 +4,7 @@
 #include "GridDescriptor.h"
 
 #include <openvdb/Exceptions.h>
-#include <boost/algorithm/string/predicate.hpp> // for boost::ends_with()
-#include <boost/algorithm/string/erase.hpp> // for boost::erase_last()
+#include <openvdb/util/Name.h>
 #include <sstream>
 
 
@@ -79,9 +78,9 @@ GridDescriptor::read(std::istream &is)
 
     // Read in the grid type.
     mGridType = readString(is);
-    if (boost::ends_with(mGridType, HALF_FLOAT_TYPENAME_SUFFIX)) {
+    if (openvdb::string::ends_with(mGridType, HALF_FLOAT_TYPENAME_SUFFIX)) {
         mSaveFloatAsHalf = true;
-        boost::erase_last(mGridType, HALF_FLOAT_TYPENAME_SUFFIX);
+        mGridType.resize(mGridType.size() - std::strlen(HALF_FLOAT_TYPENAME_SUFFIX));
     }
 
     if (getFormatVersion(is) >= OPENVDB_FILE_VERSION_GRID_INSTANCING) {

openvdb/openvdb/math/FiniteDifference.h
@@ -7,12 +7,11 @@
 #define OPENVDB_MATH_FINITEDIFFERENCE_HAS_BEEN_INCLUDED
 
 #include <openvdb/Types.h>
+#include <openvdb/util/Name.h>
 #include "Math.h"
 #include "Coord.h"
 #include "Vec3.h"
 #include <string>
-#include <boost/algorithm/string/case_conv.hpp>
-#include <boost/algorithm/string/trim.hpp>
 
 #ifdef DWA_OPENVDB
 #include <simd/Simd.h>
@@ -80,8 +79,8 @@ stringToDScheme(const std::string& s)
     DScheme ret = UNKNOWN_DS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::string::trim(str);
+    openvdb::string::to_lower(str);
 
     if (str == dsSchemeToString(CD_2NDT)) {
         ret = CD_2NDT;
@@ -194,8 +193,8 @@ stringToBiasedGradientScheme(const std::string& s)
     BiasedGradientScheme ret = UNKNOWN_BIAS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::string::trim(str);
+    openvdb::string::to_lower(str);
 
     if (str == biasedGradientSchemeToString(FIRST_BIAS)) {
         ret = FIRST_BIAS;
@@ -259,8 +258,8 @@ stringToTemporalIntegrationScheme(const std::string& s)
     TemporalIntegrationScheme ret = UNKNOWN_TIS;
 
     std::string str = s;
-    boost::trim(str);
-    boost::to_lower(str);
+    openvdb::string::trim(str);
+    openvdb::string::to_lower(str);
 
     if (str == temporalIntegrationSchemeToString(TVD_RK1)) {
         ret = TVD_RK1;

openvdb/openvdb/util/Name.h
@@ -9,6 +9,8 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 
 namespace openvdb {
 OPENVDB_USE_VERSION_NAMESPACE
@@ -34,6 +36,70 @@ writeString(std::ostream& os, const Name& name)
     os.write(reinterpret_cast<char*>(&size), sizeof(uint32_t));
     os.write(&name[0], size);
 }
+
+namespace string {
+
+template <typename ContainerT>
+inline void split(ContainerT& out, const std::string& in, const char delim)
+{
+    out.clear();
+    if (in.empty()) return;
+    std::string::size_type pos = 0u, old = 0u;
+    while ((pos = in.find(delim, pos)) != std::string::npos) {
+        out.insert(out.end(), in.substr(old, (pos-old)));
+        old = ++pos;
+    }
+    // last element
+    out.insert(out.end(), in.substr(old, in.length()-old));
+}
+
+template <typename ContainerT>
+inline void split(ContainerT& out, const std::string& in, const std::set<char>& delims)
+{
+    out.clear();
+    if (in.empty()) return;
+    std::string::size_type pos = 0u, old = 0u;
+    const std::string seq(delims.begin(), delims.end());
+    while ((pos = in.find_first_of(seq, pos)) != std::string::npos) {
+        out.insert(out.end(), in.substr(old, (pos-old)));
+        old = ++pos;
+    }
+    // last element
+    out.insert(out.end(), in.substr(old, in.length()-old));
+}
+
+inline bool starts_with(const std::string& in, const std::string& start)
+{
+    if (start.length() > in.length()) return false;
+    return std::equal(start.begin(), start.end(), in.begin());
+}
+
+inline bool ends_with(const std::string& in, const std::string& end)
+{
+    if (end.length() > in.length()) return false;
+    return std::equal(end.rbegin(), end.rend(), in.rbegin());
+}
+
+inline void trim(std::string& s)
+{
+    // ltrim
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+
+    // rtrim
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+inline void to_lower(std::string& s)
+{
+    std::transform(s.begin(), s.end(), s.begin(),
+        [](auto c){ return std::tolower(c); });
+}
+
+} // util
 
 } // namespace OPENVDB_VERSION_NAME
 } // namespace openvdb