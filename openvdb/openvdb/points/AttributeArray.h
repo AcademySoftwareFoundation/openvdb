6284cf25b6751b0fd04e396723807d51b1f1fd3d
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2023-07-04 15:26:54 +1200

    Removed unused AttributeHandle member
    Signed-off-by: Nick Avramoussis <4256455+Idclip@users.noreply.github.com>

--- openvdb/openvdb/points/AttributeArray.h
+++ openvdb/openvdb/points/AttributeArray.h
@@ -888,9 +888,6 @@ private:
     template <bool IsUnknownCodec>
     typename std::enable_if<!IsUnknownCodec, ValueType>::type get(Index index) const;
 
-    // local copy of AttributeArray (to preserve compression)
-    AttributeArray::Ptr mLocalArray;
-
     Index mStrideOrTotalSize;
     Index mSize;
     bool mCollapseOnDestruction;
