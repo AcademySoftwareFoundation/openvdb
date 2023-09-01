98176b82db8c55d7e898fef6870090cbddadd471
maletsden <maletsden@gmail.com>
2023-09-01 11:44:29 +0300

    Fixed C++20 support for OpenVDB
    Signed-off-by: maletsden <maletsden@gmail.com>

--- openvdb/openvdb/points/AttributeSet.cc
+++ openvdb/openvdb/points/AttributeSet.cc
@@ -280,7 +280,8 @@ AttributeSet::isShared(size_t pos) const
 {
     assert(pos != INVALID_POS);
     assert(pos < mAttrs.size());
-    return !mAttrs[pos].unique();
+    // Warning: In multithreaded environment, the value returned by use_count is approximate.
+    return mAttrs[pos].use_count() != 1;
 }
 
 
@@ -289,7 +290,8 @@ AttributeSet::makeUnique(size_t pos)
 {
     assert(pos != INVALID_POS);
     assert(pos < mAttrs.size());
-    if (!mAttrs[pos].unique()) {
+    // Warning: In multithreaded environment, the value returned by use_count is approximate.
+    if (mAttrs[pos].use_count() != 1) {
         mAttrs[pos] = mAttrs[pos]->copy();
     }
 }
