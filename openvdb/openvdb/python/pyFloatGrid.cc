93e03eaa74a68229422ef9e5c791ab0e4ac4b0a6
Matthew Cong <mcong@nvidia.com>
2023-10-29 00:35:22 -0700

    Port the rest of the core bindings
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

--- openvdb/openvdb/python/pyFloatGrid.cc
+++ openvdb/openvdb/python/pyFloatGrid.cc
@@ -9,7 +9,7 @@
 
 /// Create a Python wrapper for each supported Grid type.
 void
-exportFloatGrid(py::module_ m)
+exportFloatGrid(nb::module_ m)
 {
     pyGrid::exportGrid<FloatGrid>(m);
 #ifdef PY_OPENVDB_WRAP_ALL_GRID_TYPES
@@ -18,8 +18,8 @@ exportFloatGrid(py::module_ m)
 
     m.def("createLevelSetSphere",
         &pyGrid::createLevelSetSphere<FloatGrid>,
-        py::arg("radius"), py::arg("center")=openvdb::Coord(), py::arg("voxelSize")=1.0,
-             py::arg("halfWidth")=openvdb::LEVEL_SET_HALF_WIDTH,
+        nb::arg("radius"), nb::arg("center")=openvdb::Coord(), nb::arg("voxelSize")=1.0,
+             nb::arg("halfWidth")=openvdb::LEVEL_SET_HALF_WIDTH,
         "createLevelSetSphere(radius, center, voxelSize, halfWidth) -> FloatGrid\n\n"
         "Return a grid containing a narrow-band level set representation\n"
         "of a sphere.");
