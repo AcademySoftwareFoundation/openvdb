ae98d096c81baadff926839904e43867a55545cb
Luke Titley <from+badja@luketitley.com>
2023-11-18 19:22:53 +0100

    A bit more work on enumerable_thread_specific implementation

--- openvdb/openvdb/mt_naive/combinable.h
+++ openvdb/openvdb/mt_naive/combinable.h
@@ -9,15 +9,78 @@
 
 #include <openvdb/version.h>
 
-#include <tbb/combinable.h>
+#include <unordered_map>
+#include <shared_mutex>
 
 namespace openvdb {
 OPENVDB_USE_VERSION_NAMESPACE
 namespace OPENVDB_VERSION_NAME {
 namespace mt {
 
+#if 1
+
 template <typename T>
 using combinable = ::tbb::combinable<T>;
+
+#else
+
+/** \name combinable
+**/
+//@{
+//! Thread-local storage with optional reduction
+/** @ingroup containers */
+template <typename T>
+class combinable {
+
+private:
+    my_ets_type;
+    my_ets_type my_ets;
+
+public:
+
+    combinable() { }
+
+    template <typename finit>
+    explicit combinable( finit _finit) : my_ets(_finit) { }
+
+    //! destructor
+    ~combinable() { }
+
+    combinable( const combinable& other) : my_ets(other.my_ets) { }
+
+#if __TBB_ETS_USE_CPP11
+    combinable( combinable&& other) : my_ets( std::move(other.my_ets)) { }
+#endif
+
+    combinable & operator=( const combinable & other) {
+        my_ets = other.my_ets;
+        return *this;
+    }
+
+#if __TBB_ETS_USE_CPP11
+    combinable & operator=( combinable && other) {
+        my_ets=std::move(other.my_ets);
+        return *this;
+    }
+#endif
+
+    void clear() { my_ets.clear(); }
+
+    T& local() { return my_ets.local(); }
+
+    T& local(bool & exists) { return my_ets.local(exists); }
+
+    // combine_func_t has signature T(T,T) or T(const T&, const T&)
+    template <typename combine_func_t>
+    T combine(combine_func_t f_combine) { return my_ets.combine(f_combine); }
+
+    // combine_func_t has signature void(T) or void(const T&)
+    template <typename combine_func_t>
+    void combine_each(combine_func_t f_combine) { my_ets.combine_each(f_combine); }
+
+};
+
+#endif
 
 } // mt
 } // OPENVDB_VERSION_NAME
--- openvdb/openvdb/mt_naive/enumerable_thread_specific.h
+++ openvdb/openvdb/mt_naive/enumerable_thread_specific.h
@@ -8,18 +8,200 @@
 #define OPENVDB_ENUMERABLE_THREAD_SPECIFIC_HAS_BEEN_INCLUDED
 
 #include <openvdb/version.h>
+#include <cassert>
 
-#include <tbb/enumerable_thread_specific.h>
+//#include <tbb/enumerable_thread_specific.h>
+#include <thread>
+#include <shared_mutex>
 
 namespace openvdb {
 OPENVDB_USE_VERSION_NAMESPACE
 namespace OPENVDB_VERSION_NAME {
 namespace mt {
 
-template <typename T,
-              typename Allocator=::tbb::cache_aligned_allocator<T>,
-              ::tbb::ets_key_usage_type ETS_key_type=::tbb::ets_no_key >
-using enumerable_thread_specific = ::tbb::enumerable_thread_specific<T, Allocator, ETS_key_type>;
+template <typename T, typename Values>
+class enumerable_thread_specific_iterator_base
+{
+public:
+    using Self = enumerable_thread_specific_iterator_base<T, Values>;
+
+    enumerable_thread_specific_iterator_base(std::shared_mutex & lock, Values & locals, size_t i)
+        : m_lock(lock)
+        , m_locals(locals)
+        , m_i(i)
+    {}
+
+    bool operator==(const Self & rhs) const
+    {
+        assert(&m_lock == &rhs.m_lock);
+        assert(&m_locals == &rhs.m_locals);
+        return m_i == rhs.m_i;
+    }
+
+    bool operator!=(const Self & rhs) const
+    {
+        assert(&m_lock == &rhs.m_lock);
+        assert(&m_locals == &rhs.m_locals);
+        return m_i != rhs.m_i;
+    }
+
+    bool operator!=(const T & rhs) const
+    {
+        assert(&m_lock == &rhs.m_lock);
+        assert(&m_locals == &rhs.m_locals);
+
+        std::shared_lock lock(m_lock);
+        auto it = m_locals.begin()+m_i;
+        return it->second != rhs;
+    }
+
+    Self & operator++()
+    {
+        ++m_i;
+    }
+
+    Self & operator++(int)
+    {
+        ++m_i;
+    }
+
+protected:
+    std::shared_mutex & m_lock;
+    Values & m_locals;
+    size_t m_i;
+};
+
+template <typename T, typename Values>
+class enumerable_thread_specific_iterator
+    : public enumerable_thread_specific_iterator_base<T, Values>
+{
+public:
+    using Base = enumerable_thread_specific_iterator_base<T, Values>;
+    using Base::Base;
+
+    T & operator*()
+    {
+        std::shared_lock lock(Base::m_lock);
+        auto it = Base::m_locals.begin()+Base::m_i;
+        return it->second;
+    }
+};
+
+template <typename T, typename Values>
+class enumerable_thread_specific_const_iterator
+    : public enumerable_thread_specific_iterator_base<T, Values>
+{
+public:
+    using Base = enumerable_thread_specific_iterator_base<T, Values>;
+    using Base::Base;
+
+    const T & operator*() const
+    {
+        std::shared_lock lock(Base::m_lock);
+        auto it = Base::m_locals.begin()+Base::m_i;
+        return it->second;
+    }
+};
+
+template <typename T>
+class enumerable_thread_specific
+{
+public:
+    using Values = std::unordered_map<std::thread::id, T>;
+    using iterator = enumerable_thread_specific_iterator<T, Values>;
+    using const_iterator = enumerable_thread_specific_const_iterator<T, const Values>;
+
+    iterator begin()
+    {
+        return iterator(m_lock, m_locals, 0);
+    }
+
+    iterator end()
+    {
+        std::shared_lock lock(m_lock);
+        return iterator(m_lock, m_locals, m_locals.size());
+    }
+
+    const_iterator begin() const
+    {
+        return const_iterator(m_lock, m_locals, 0);
+    }
+
+    const_iterator end() const
+    {
+        std::shared_lock lock(m_lock);
+        return const_iterator(m_lock, m_locals, m_locals.size());
+    }
+
+    bool empty()
+    {
+        std::shared_lock lock(m_lock);
+        return m_locals.empty();
+    }
+
+    size_t size() const
+    {
+        std::shared_lock lock(m_lock);
+        return m_locals.size();
+    }
+
+    void clear()
+    {
+        std::unique_lock lock(m_lock);
+        m_locals.clear();
+    }
+
+    T& local(bool & exists)
+    {
+        std::thread::id id = std::this_thread::get_id();
+
+        // The fast path, find the entry if it exists
+        // and just return it
+        {
+            std::shared_lock lock(m_lock);
+            typename Values::iterator it = m_locals.find(id);
+            if( it != m_locals.end() )
+            {
+                exists = true;
+                return it->second;
+            }
+        }
+
+        // The slow path
+        {
+            std::unique_lock lock(m_lock);
+            auto result = m_locals.emplace(id, T());
+            exists = false;
+            return result.first->second;
+        }
+    }
+
+    T& local()
+    {
+        bool exists = false;
+        return local(exists);
+    }
+
+    // combine_func_t has signature T(T,T) or T(const T&, const T&)
+    template <typename combine_func_t>
+    T combine(combine_func_t f_combine)
+    {
+        std::shared_lock lock(m_lock);
+        return std::reduce( m_locals.begin(), m_locals.end(), f_combine );
+    }
+
+    // combine_func_t has signature void(T) or void(const T&)
+    template <typename combine_func_t>
+    void combine_each(combine_func_t f_combine)
+  {
+        std::shared_lock lock(m_lock);
+        std::for_each( m_locals.begin(), m_locals.end(), f_combine );
+    }
+
+private:
+    mutable std::shared_mutex m_lock;
+    Values m_locals;
+};
 
 } // mt
 } // OPENVDB_VERSION_NAME
