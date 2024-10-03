7aa4136886f3141a56f7bc96ffc9dafe9890baf8
Tim Straubinger <tstraubinger@ilm.com>
2024-10-03 15:54:29 -0700

    Convert TestFunctionRegistry.cc from cppunit to gtest
    Signed-off-by: Tim Straubinger <tstraubinger@ilm.com>

--- openvdb_ax/openvdb_ax/test/backend/TestFunctionRegistry.cc
+++ openvdb_ax/openvdb_ax/test/backend/TestFunctionRegistry.cc
@@ -8,23 +8,14 @@
 #include <openvdb_ax/compiler/CompilerOptions.h>
 #include <openvdb_ax/codegen/Functions.h>
 #include <openvdb_ax/codegen/FunctionRegistry.h>
-#include <cppunit/extensions/HelperMacros.h>
 
-class TestFunctionRegistry : public CppUnit::TestCase
+#include <gtest/gtest.h>
+
+class TestFunctionRegistry : public ::testing::Test
 {
-public:
-
-    CPPUNIT_TEST_SUITE(TestFunctionRegistry);
-    CPPUNIT_TEST(testCreateAllVerify);
-    CPPUNIT_TEST_SUITE_END();
-
-    void testCreateAllVerify();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestFunctionRegistry);
-
-void
-TestFunctionRegistry::testCreateAllVerify()
+TEST_F(TestFunctionRegistry, testCreateAllVerify)
 {
     openvdb::ax::codegen::FunctionRegistry::UniquePtr reg =
         openvdb::ax::codegen::createDefaultRegistry();
@@ -41,7 +32,7 @@ TestFunctionRegistry::testCreateAllVerify()
         std::cerr.rdbuf(buffer.rdbuf());
         reg->createAll(opts, true);
         const std::string& result = buffer.str();
-        CPPUNIT_ASSERT_MESSAGE(result, result.empty());
+        ASSERT_TRUE(result.empty()) << result;
     }
     catch (...) {
         std::cerr.rdbuf(sbuf);
