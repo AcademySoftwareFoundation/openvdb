748ace48cd678a904d93f9157cb7ee2d3c35527e
Tim Straubinger <tstraubinger@ilm.com>
2024-10-18 13:18:56 -0700

    Convert TestArrayPack.cc to use gtest; add temporary support for both gtest and cppunit to test/util.h
    Signed-off-by: Tim Straubinger <tstraubinger@ilm.com>

--- openvdb_ax/openvdb_ax/test/frontend/TestArrayPack.cc
+++ openvdb_ax/openvdb_ax/test/frontend/TestArrayPack.cc
@@ -8,7 +8,7 @@
 
 #include "../util.h"
 
-#include <cppunit/extensions/HelperMacros.h>
+#include <gtest/gtest.h>
 
 #include <string>
 
@@ -116,34 +116,29 @@ static const unittest_util::CodeTests tests =
 
 }
 
-class TestArrayPack : public CppUnit::TestCase
+class TestArrayPack : public ::testing::Test
 {
-public:
-
-    CPPUNIT_TEST_SUITE(TestArrayPack);
-    CPPUNIT_TEST(testSyntax);
-    CPPUNIT_TEST(testASTNode);
-    CPPUNIT_TEST_SUITE_END();
-
     void testSyntax() { TEST_SYNTAX_PASSES(tests); }
     void testASTNode();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestArrayPack);
+TEST_F(TestArrayPack, testSyntax)
+{
+    TEST_SYNTAX_PASSES(tests);
+}
 
-void TestArrayPack::testASTNode()
+TEST_F(TestArrayPack, testASTNode)
 {
     for (const auto& test : tests) {
         const std::string& code = test.first;
         const Node* expected = test.second.get();
         const Tree::ConstPtr tree = parse(code.c_str());
-        CPPUNIT_ASSERT_MESSAGE(ERROR_MSG("No AST returned", code), static_cast<bool>(tree));
+        ASSERT_TRUE(static_cast<bool>(tree)) << ERROR_MSG("No AST returned", code);
 
         // get the first statement
         const Node* result = tree->child(0)->child(0);
-        CPPUNIT_ASSERT(result);
-        CPPUNIT_ASSERT_MESSAGE(ERROR_MSG("Invalid AST node", code),
-            Node::ArrayPackNode == result->nodetype());
+        ASSERT_TRUE(result);
+        ASSERT_TRUE(Node::ArrayPackNode == result->nodetype()) << ERROR_MSG("Invalid AST node", code);
 
         std::vector<const Node*> resultList, expectedList;
         linearize(*result, resultList);
@@ -155,7 +150,7 @@ void TestArrayPack::testASTNode()
             openvdb::ax::ast::print(*expected, true, os);
             os << "Result:\n";
             openvdb::ax::ast::print(*result, true, os);
-            CPPUNIT_FAIL(ERROR_MSG("Mismatching Trees for Array Pack code", code) + os.str());
+            FAIL() << ERROR_MSG("Mismatching Trees for Array Pack code", code) + os.str();
         }
     }
 }
