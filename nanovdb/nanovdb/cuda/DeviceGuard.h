93293494616c3cd0e6dd75b5af2d790d9bc53037
Matthew Cong <1372750+matthewdcong@users.noreply.github.com>
2025-01-15 13:41:11 -0800

    Add DeviceMesh and (optional) NCCL CMake Support
    * Add device mesh
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Restore current device in constructor and destructor
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Enable range-based for loops
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Use structured bindings inside for loop
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Remove uneeded accessors and modularize functions
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Statically initialize entry point
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Encapsulate
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Switch to lightweight wrapper class for DeviceNodes vector
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Cleanup
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Minor changes needed for DistributedPointsToGrid
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Add RAII DeviceGuard
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * More cleanup
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Clean up NCCL dependency
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Remove parallel_for to modularize
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Implement and test move constructor/assignment
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Add docs
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Expand test and documentation
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    * Move non-reinit comment to function description
    
    Co-authored-by: Mark Harris <783069+harrism@users.noreply.github.com>
    
    * Switch to using aliases and modern type names
    
    Co-authored-by: Mark Harris <783069+harrism@users.noreply.github.com>
    
    * Use size_type instead of int for return
    
    Co-authored-by: Mark Harris <783069+harrism@users.noreply.github.com>
    
    * Separate out DeviceGuard and clean up API
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    
    ---------
    
    Signed-off-by: Matthew Cong <mcong@nvidia.com>
    Co-authored-by: Mark Harris <783069+harrism@users.noreply.github.com>
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

---
+++ nanovdb/nanovdb/cuda/DeviceGuard.h
@@ -0,0 +1,39 @@
+// Copyright Contributors to the OpenVDB Project
+// SPDX-License-Identifier: Apache-2.0
+
+/*!
+    \file DeviceGuard.h
+
+    \brief nanovdb::cuda::DeviceGuard is an RAII class for caching/restoring the
+    current device associated with the host thread.
+*/
+
+#ifndef NANOVDB_CUDA_DEVICEGUARD_H_HAS_BEEN_INCLUDED
+#define NANOVDB_CUDA_DEVICEGUARD_H_HAS_BEEN_INCLUDED
+
+#include <cuda_runtime_api.h>
+
+namespace nanovdb {
+
+namespace cuda {
+
+/// @brief RAII class that caches/restores the current device at construction/destruction
+class DeviceGuard {
+    public:
+        DeviceGuard() { cudaGetDevice(&deviceId); }
+        ~DeviceGuard() { cudaSetDevice(deviceId); }
+
+        // DeviceGuard is not copyable nor movable
+        DeviceGuard(const DeviceGuard&) = delete;
+        DeviceGuard& operator=(const DeviceGuard&) = delete;
+        DeviceGuard(DeviceGuard&& other) = delete;
+        DeviceGuard& operator=(DeviceGuard&& other) = delete;
+    private:
+        int deviceId = -1;
+};
+
+}
+
+} // namespace nanovdb::cuda
+
+#endif // end of NANOVDB_CUDA_DEVICEGUARD_H_HAS_BEEN_INCLUDED
