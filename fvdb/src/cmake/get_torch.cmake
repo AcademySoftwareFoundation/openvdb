86f2734cd2030b961ca77d478a034ff9e7395776
Mark Harris <mharris@nvidia.com>
2025-01-23 09:38:53 +1100

    Fix conda envs and cmake for unit tests (#153)
    * Fix build- and dev- environments and cmake so unit tests will build with conda-forge packages
    
    Signed-off-by: Mark Harris <mharris@nvidia.com>
    
    * Remove no-longer-needed env
    
    Signed-off-by: Mark Harris <mharris@nvidia.com>
    
    ---------
    
    Signed-off-by: Mark Harris <mharris@nvidia.com>

--- fvdb/src/cmake/get_torch.cmake
+++ fvdb/src/cmake/get_torch.cmake
@@ -1,16 +1,8 @@
 # Copyright Contributors to the OpenVDB Project
 # SPDX-License-Identifier: Apache-2.0
 
-# find Python3 and site-packages path
+# find Python3
 find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
-execute_process(
-  COMMAND "${Python3_EXECUTABLE}" -c "if True:
-    from distutils import sysconfig as sc
-    print(sc.get_python_lib(prefix='', plat_specific=True))"
-  OUTPUT_VARIABLE PYTHON_SITE
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-set (PYTHON_SITE "${CONDA_ENV_PATH}/${PYTHON_SITE}")
 
 # Check that PyTorch package uses the C++11 ABI
 execute_process(
@@ -23,8 +15,6 @@ if (NOT TORCH_CXX11_ABI)
                         "Please install PyTorch with the C++11 ABI (e.g. conda-forge package).")
 endif()
 
-# find torch, looking in site-packages
-set(Torch_DIR ${PYTHON_SITE}/torch/share/cmake/Torch)
 # needed to correctly configure Torch with the conda-forge build
 set(CUDA_TOOLKIT_ROOT_DIR "${CONDA_ENV_PATH}/targets/x86_64-linux")
 find_package(Torch REQUIRED)
