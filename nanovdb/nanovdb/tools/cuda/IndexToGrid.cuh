fd63b9aca13414c2a964e72202a78ca3db86b500
Ken <ken.museth@gmail.com>
2025-04-01 12:30:40 -0700

    minor update to documentation
    Signed-off-by: Ken <ken.museth@gmail.com>

--- nanovdb/nanovdb/tools/cuda/IndexToGrid.cuh
+++ nanovdb/nanovdb/tools/cuda/IndexToGrid.cuh
@@ -188,10 +188,10 @@ __global__ void processRootTilesKernel(typename IndexToGrid<SrcBuildT>::NodeAcce
         // |<-------------------- offset -------------------|
         // |<-------------------------------- Tile::child ------------------------------------|
         //                                                  |<------ Tile::child-offset ----->|
-        //                                                  |<--- ChildID x sizeof(ChildT) -->|
-        uint64_t offset = sizeof(NanoRoot<SrcBuildT>) + tileCount*sizeof(NanoRoot<SrcBuildT>::Tile);// source offset
+        //                                                  |<--- childID x sizeof(ChildT) -->|
+        uint64_t offset = sizeof(NanoRoot<SrcBuildT>) + tileCount*sizeof(NanoRoot<SrcBuildT>::Tile);//  source offset
         const uint64_t childID = (srcTile.child - offset)/sizeof(NanoRoot<SrcBuildT>::ChildNodeType);// derived from source offset
-        offset          = sizeof(NanoRoot<DstBuildT>) + tileCount*sizeof(NanoRoot<DstBuildT>::Tile);// destination offset
+        offset          = sizeof(NanoRoot<DstBuildT>) + tileCount*sizeof(NanoRoot<DstBuildT>::Tile);//  destination offset
         dstTile.child   = offset + childID*sizeof(NanoRoot<DstBuildT>::ChildNodeType);
         dstTile.value   = srcValues[0];// set to background
         dstTile.state   = false;
