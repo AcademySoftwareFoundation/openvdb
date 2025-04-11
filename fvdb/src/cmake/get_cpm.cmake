868ef3a1eb5ac1b8153acc9eab56a3b3dae6194f
Jonathan Swartz <jonathan@jswartz.info>
2025-04-11 17:18:29 +1200

    Add global cmake download timeout and CPM_SOURCE_CACHE to fvdb CI(#328)
    ---------
    
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- fvdb/src/cmake/get_cpm.cmake
+++ fvdb/src/cmake/get_cpm.cmake
@@ -14,9 +14,10 @@ if(NOT EXISTS ${CPM_SOURCECODE_DIR}/CPM.cmake)
     https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake
     ${CPM_SOURCECODE_DIR}/CPM.cmake
     EXPECTED_HASH SHA256=c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d
+    TIMEOUT 600
+    INACTIVITY_TIMEOUT 60
   )
 endif()
 
 # Include CPM.cmake from the cache
 include(${CPM_SOURCECODE_DIR}/CPM.cmake)
-
