f2a4b92dc850ebae4348fc5c028f2b1f31e9c6d2
Ken <ken.museth@gmail.com>
2025-10-22 09:58:18 -0700

    improved documentation
    Signed-off-by: Ken <ken.museth@gmail.com>

--- nanovdb/nanovdb/cuda/TempPool.h
+++ nanovdb/nanovdb/cuda/TempPool.h
@@ -1,6 +1,12 @@
 // Copyright Contributors to the OpenVDB Project
 // SPDX-License-Identifier: Apache-2.0
 //
+/// @brief Defines a simple memory pool used to call cub functions that use dynamic temporary storage
+///
+/// @details See nanovdb/tools/cuda/PointToGrid.cuh and nanovdb/tools/cuda/DistributedPointToGrid.cuh
+///          for examples. Also note that this explains the somewhat unusual API with direct access to
+///          protected member data.
+//
 #ifndef NANOVDB_CUDA_TEMPPOOL_H_HAS_BEEN_INCLUDED
 #define NANOVDB_CUDA_TEMPPOOL_H_HAS_BEEN_INCLUDED
 
@@ -13,7 +19,11 @@ namespace cuda {
 template <class Resource>
 class TempPool {
 public:
+
+    /// @brief Default c-tor of an empty memory pool.
     TempPool() : mData(nullptr), mSize(0), mRequestedSize(0) {}
+    
+    /// @brief Destructor
     ~TempPool() {
         mRequestedSize = 0;
         Resource::deallocateAsync(mData, mSize, Resource::DEFAULT_ALIGNMENT, nullptr);
@@ -21,18 +31,19 @@ public:
         mSize = 0;
     }
 
-    void* data() {
-        return mData;
-    }
+    /// @brief Returns a non-const void pointer to the data managed by this instance.
+    void* data() {return mData;}
 
-    size_t& size() {
-        return mSize;
-    }
+    /// @brief Returns a non-const reference to the actual size of the data managed by this instance.
+    size_t& size() {return mSize;}
 
-    size_t& requestedSize() {
-        return mRequestedSize;
-    }
+    /// @brief Returns a non-const reference to the requested size of the data managed by this instance.
+    /// @note This requested size should always be less than or smaller than the actual size().
+    size_t& requestedSize() {return mRequestedSize;}
 
+    /// @brief Re-allocation of the data managed by this instance. Only has affect if the pool in empty or
+    ///        the requested memory is larger than the existing size.
+    /// @param stream cuda stream used for asynchronous de-allocation and allocation.
     void reallocate(cudaStream_t stream) {
         if (!mData || mRequestedSize > mSize) {
             Resource::deallocateAsync(mData, mSize, Resource::DEFAULT_ALIGNMENT, stream);
@@ -41,15 +52,15 @@ public:
         }
     }
 private:
-    void* mData;
+    void  *mData;
     size_t mSize;
     size_t mRequestedSize;
-};
+};// TempPool<Resource> class
 
 using TempDevicePool = TempPool<DeviceResource>;
 
-}
+} // namespace cuda
 
-} // namespace nanovdb::cuda
+} // namespace nanovdb
 
 #endif // end of NANOVDB_CUDA_TEMPPOOL_H_HAS_BEEN_INCLUDED
