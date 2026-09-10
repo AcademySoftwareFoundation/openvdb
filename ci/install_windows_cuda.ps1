b1a4fa5dab9aaca7298524c01f7e5a61fe8749c3
Jonathan Swartz <jonathan@jswartz.info>
2026-09-10 14:17:25 +1200

    Update CUDA version to 12.9.1 in CI configuration and adjust build steps to remove unnecessary sudo commands.
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- ci/install_windows_cuda.ps1
+++ ci/install_windows_cuda.ps1
@@ -28,7 +28,7 @@ $CUDA_KNOWN_URLS = @{
     "11.3.0" = "https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe";
     "11.6.2" = "https://developer.download.nvidia.com/compute/cuda/11.6.2/network_installers/cuda_11.6.2_windows_network.exe";
     "12.4.0" = "https://developer.download.nvidia.com/compute/cuda/12.4.0/network_installers/cuda_12.4.0_windows_network.exe";
-    "12.9.2" = "https://developer.download.nvidia.com/compute/cuda/12.9.2/network_installers/cuda_12.9.2_windows_network.exe";
+    "12.9.1" = "https://developer.download.nvidia.com/compute/cuda/12.9.1/network_installers/cuda_12.9.1_windows_network.exe";
     "13.2.2" = "https://developer.download.nvidia.com/compute/cuda/13.2.2/network_installers/cuda_13.2.2_windows_network.exe"
 }
 
